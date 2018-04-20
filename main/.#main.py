30bce8bdb8f7a516601521c7ecd65bdd364a7c4d
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-04-20 10:53:47 +0900

    とりあえず、バックアップ

---
+++ main/.#main.py
@@ -0,0 +1,1 @@
+miyo@gwdhcp029.icrr.u-tokyo.ac.jp.5619
--- main/main.py
+++ main/main.py
@@ -28,7 +28,12 @@ def R(theta):
         [0.,0.,1.]
     ])
     return mat
-
+if False:   
+    a = np.array([1,0,0]).T
+    print a
+    b = np.dot(R(45),a)
+    print b
+    exit()
 
 def main_getGPStime():
     '''イベントを指定して、そのイベントの期間を返す関数。    
@@ -58,10 +63,17 @@ def main_getGPStime():
         'Example_Night_Long':[1207213218,2**15], # 4/08 24:00
         'Example_Long_1day':[1207440018,2**16], # 04/11 09:00, 18hour
         #'Example_Long_1day':[1207440018,2**10], # 04/11 09:00, 18hour
-        '0416_00h00m':[1207839618,2**14],
-        '0416_00h00m':[1207839618,2**12],
         '0413_20h00m':[1207652418,2**14],
-        '0413_20h00m':[1207652418,2**13],
+        '0413_20h00m':[1207652418,2**13], # 2**13 Ok KAGRA,GIF, 2**16made
+        '0416_00h00m':[1207839618,2**13],        #2**15made
+        '0416_13h00m':[1207886418,2**13], # 2**13 ok 
+        '0416_16h00m':[1207897218,2**13], # OK
+        '0416_19h00m':[1207908018,2**13], # OK
+        '0416_22h00m':[1207918818,2**13], # OK
+        '0417_01h00m':[1207929618,2**13], # OK 
+        '0417_17h00m':[1207987218,2**13], # OK,2**16 made
+        '0417_20h00m':[1207998018,2**13], # OK ,2**16 made
+        '0417_23h00m':[1208008818,2**13], # OK ,2**16 made
         #'hoge':[1207440018,2**6], # 04/11 09:00, 18hour
     }
     try:
@@ -83,71 +95,6 @@ def main_getGPStime():
         exit()
     return start,tlen,title,option
 
-def getKAGRAseismometer(start,tlen,title,option,theta=0.0,plot33=False,after_28Mar=True,after_12Mar=False,do_you_want_to_calib=True):    
-    '''
-    3つの地震計の3つの軸の信号を取得してくる。
-    '''
-    fs = 8.0    
-    EX1_SEIS = reader.kagra(start,tlen,['K1:PEM-EX1_SEIS_NS_SENSINF_OUT16','K1:PEM-EX1_SEIS_WE_SENSINF_OUT16','K1:PEM-EX1_SEIS_Z_SENSINF_OUT16']).T    
-    EY1_SEIS = reader.kagra(start,tlen,['K1:PEM-EY1_SEIS_WE_SENSINF_OUT16','K1:PEM-EY1_SEIS_NS_SENSINF_OUT16','K1:PEM-EY1_SEIS_Z_SENSINF_OUT16']).T
-    IY0_SEIS = reader.kagra(start,tlen,['K1:PEM-IY0_SEIS_NS_SENSINF_OUT16','K1:PEM-IY0_SEIS_WE_SENSINF_OUT16','K1:PEM-IY0_SEIS_Z_SENSINF_OUT16']).T
-    if do_you_want_to_calib:
-        v = 5500.0
-        EX1_SEIS = EX1_SEIS/v
-        EY1_SEIS = EY1_SEIS/v
-        IY0_SEIS = IY0_SEIS/v    
-    if after_28Mar:
-        # 3/28 -  , all 210 deg rotation to X arm        
-        Xend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EX1_SEIS))
-        Yend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EY1_SEIS))
-        Cent_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),IY0_SEIS))
-    if after_12Mar:
-        # 3/12- 3/28 , Y:180,X:90,C:90.
-        Xend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EX1_SEIS))
-        Yend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EY1_SEIS))
-        Cent_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),IY0_SEIS))    
-    # Rotate EW axis to X-arm.
-    Xend_Xarm, Xend_Yarm, Xend_Z = Xend_NSEW.T
-    Cent_Xarm, Cent_Yarm, Cent_Z = Cent_NSEW.T
-    Yend_Xarm, Yend_Yarm, Yend_Z = Yend_NSEW.T
-    time = np.arange(len(Xend_Xarm))/fs   
-    # for plotting 3x3 plot
-    if plot33:
-        data = [[time,Xend_Xarm],[time,Xend_Yarm],[time,Xend_Z],
-                [time,Cent_Xarm],[time,Cent_Yarm],[time,Cent_Z],
-                [time,Yend_Xarm],[time,Yend_Yarm],[time,Yend_Z]]    
-        label = ['Xend2F_Xarm','Xend2F_Yarm','Xend2F_Z',
-                'Cent1F_Xarm','Cent1F_Yarm','Cent1F_Z',
-                'Yend2F_Xarm','Yend2F_Yarm','Yend2F_Z']
-        fname = '{0}/{1}_{2}_{3}.png'.format(title,start,tlen,option)
-        cmd = 'mkdir -p {0}'.format(title)
-        print cmd
-        ret  =  subprocess.check_call( cmd.split(" ") )
-        mpplot.subplot33(data,fname,label)
-        exit()
-    else:
-        Yend_Xarm = mptypes.Timeseries(Yend_Xarm,fs=8,name='Yend_x')
-        Xend_Xarm = mptypes.Timeseries(Xend_Xarm,fs=8,name='Xend_x')
-        Cent_Xarm = mptypes.Timeseries(Cent_Xarm,fs=8,name='Cent_x')
-        Yend_Xarm.get_psd(ave=8,plot=False)
-        Xend_Xarm.get_psd(ave=8,plot=False)
-        Cent_Xarm.get_psd(ave=8,plot=False)
-        #
-        Yend_Yarm = mptypes.Timeseries(Yend_Yarm,fs=8,name='Yend_x')
-        Xend_Yarm = mptypes.Timeseries(Xend_Yarm,fs=8,name='Xend_x')
-        Cent_Yarm = mptypes.Timeseries(Cent_Yarm,fs=8,name='Cent_x')
-        Yend_Yarm.get_psd(ave=8,plot=False)
-        Xend_Yarm.get_psd(ave=8,plot=False)
-        Cent_Yarm.get_psd(ave=8,plot=False)
-        #
-        Yend_Z = mptypes.Timeseries(Yend_Z,fs=8,name='Yend_x')
-        Xend_Z = mptypes.Timeseries(Xend_Z,fs=8,name='Xend_x')
-        Cent_Z = mptypes.Timeseries(Cent_Z,fs=8,name='Cent_x')
-        Yend_Z.get_psd(ave=8,plot=False)
-        Xend_Z.get_psd(ave=8,plot=False)
-        Cent_Z.get_psd(ave=8,plot=False)        
-        print 'get KAGRA data'
-        return [[Xend_Xarm,Yend_Xarm,Cent_Xarm],[Xend_Yarm,Yend_Yarm,Cent_Yarm],[Xend_Z,Yend_Z,Cent_Z]]
 
 def getGIFdata(start,tlen,title,option,theta=0.0,plot33=False,after_28Mar=True,after_12Mar=False,do_you_want_to_calib=True):
     TR240_EW = reader.gif(start,tlen,'X1500_TR240velEW',)
@@ -156,23 +103,25 @@ def getGIFdata(start,tlen,title,option,theta=0.0,plot33=False,after_28Mar=True,a
     v2hPa = 1.0
     v2cdegree = 1.0
     #
-    Strain = reader.gif(start,tlen,'CALC_STRAIN',)
+    Strain = reader.gif(start,tlen,'CALC_STRAIN',)*1.5e3
     Baro_x500 = reader.gif(start,tlen,'X500_BARO',)*v2hPa*-2e-8
     Baro_x2000 = reader.gif(start,tlen,'X2000_BARO',)*v2hPa*2e-8
     Temp_x500 = reader.gif(start,tlen,'X500_TEMP',)*v2cdegree
     Temp_x2000 = reader.gif(start,tlen,'X2000_TEMP',)*v2cdegree
     TR240_SEIS = np.array([TR240_EW,TR240_NS,TR240_UD]).T
-    if do_you_want_to_calib:
+    if True:
         v = 5500.0
+        v = 1
         TR240_SEIS = TR240_SEIS/v/1196.5            
     TR240_NSEW = np.array(map(lambda x:np.dot(x,R(-30+theta)),TR240_SEIS))
-    TR240_Xarm, TR240_Yarm, TR240_Z = TR240_NSEW.T
-    do_you_want_to_calib = True
+    TR240_x, TR240_y, TR240_z = TR240_NSEW.T
     #
     Strain = mptypes.Timeseries(Strain,fs=8,name='GIF_X',plot=True)
-    TR240_Xarm = mptypes.Timeseries(TR240_Xarm,fs=8,name='X1500_x',plot=True)
-    TR240_Yarm = mptypes.Timeseries(TR240_Yarm,fs=8,name='X1500_y',plot=True)
-    TR240_Z = mptypes.Timeseries(TR240_Z,fs=8,name='X1500_z',plot=True)
+    TR240_x = mptypes.Timeseries(TR240_x,fs=8,name='X1500_x',plot=True)
+    TR240_y = mptypes.Timeseries(TR240_y,fs=8,name='X1500_y',plot=True)
+    TR240_z = mptypes.Timeseries(TR240_z,fs=8,name='X1500_z',plot=True)
+    CMRR_timeseries = Xend_x.timeseries - Cent_x.timeseries
+    CMRR = mptypes.Timeseries(CMRR_timeseries,fs=8,name='CMRR',plot=True)
     Baro_x500 = mptypes.Timeseries(Baro_x500,fs=8,name='X500_Pres',plot=True)
     Baro_x2000 = mptypes.Timeseries(Baro_x2000,fs=8,name='X2000_Pres',plot=True)
     Temp_x500 = mptypes.Timeseries(Temp_x500,fs=8,name='X500_Temp',plot=True)
@@ -182,33 +131,82 @@ def getGIFdata(start,tlen,title,option,theta=0.0,plot33=False,after_28Mar=True,a
     Baro_x500.get_psd(ave=8,plot=False)
     Baro_x2000.get_psd(ave=8,plot=False)
     Temp_x500.get_psd(ave=8,plot=False)
-    Temp_x2000.get_psd(ave=8,plot=False)    
-    TR240_Xarm.get_psd(ave=8,plot=False)
-    TR240_Yarm.get_psd(ave=8,plot=False)
-    TR240_Z.get_psd(ave=8,plot=False)    
-    return Strain,TR240_Xarm,Baro_x500,Baro_x2000,Temp_x500,Temp_x2000
+    Temp_x2000.get_psd(ave=8,plot=False)
+    CMRR.get_psd(ave=8,plot=False,integ=False)
+    TR240_x.get_psd(ave=8,plot=False,integ=True)
+    TR240_y.get_psd(ave=8,plot=False,integ=False)
+    TR240_z.get_psd(ave=8,plot=False,integ=False)    
+    return Strain,TR240_x,Baro_x500,Baro_x2000,Temp_x500,Temp_x2000,CMRR,CMRR_
 
 
-def coherence(x,y):
-    import matplotlib.pyplot as plt
-    fs = 8
-    ave = 8.0
-    fname = '{0}-{1}'.format(x._name,y._name)
-    f, Cxy = signal.coherence(x.timeseries, y.timeseries, fs, nperseg=len(x.timeseries)/ave)    
-    plt.semilogx(f, Cxy,label='{0} / {1}'.format(y._name,x._name))
-    #
-    clfunc = lambda a: 1.0-(1.0-a/100.0)**(1./(ave-1.0))            
-    cl = 99.7 # %    
-    plt.plot(f,np.ones(len(f))*clfunc(cl),'k--',linewidth=1,alpha=0.5)
-    plt.text(f[1], clfunc(cl)*0.9, '{0:3.2f}%'.format(cl),bbox={'facecolor':'w', 'alpha':0.9, 'pad':0.5},alpha=0.5)
-    #
-    plt.xlabel('frequency [Hz]')
-    plt.ylabel('Coherence')
-    plt.legend()
-    plt.ylim(0,1)
-    plt.savefig('Coherence_{0}.png'.format(fname))
-    plt.close()    
-
+def getKAGRAseismometer(start,tlen,title,option,theta=0.0,plot33=False,after_28Mar=True,after_12Mar=False,do_you_want_to_calib=True):    
+    '''
+    3つの地震計の3つの軸の信号を取得してくる。
+    '''
+    fs = 8.0    
+    EX1_SEIS = reader.kagra(start,tlen,['K1:PEM-EX1_SEIS_WE_SENSINF_OUT16','K1:PEM-EX1_SEIS_NS_SENSINF_OUT16','K1:PEM-EX1_SEIS_Z_SENSINF_OUT16']).T    
+    IY0_SEIS = reader.kagra(start,tlen,['K1:PEM-IY0_SEIS_WE_SENSINF_OUT16','K1:PEM-IY0_SEIS_NS_SENSINF_OUT16','K1:PEM-IY0_SEIS_Z_SENSINF_OUT16']).T
+    EY1_SEIS = reader.kagra(start,tlen,['K1:PEM-EY1_SEIS_WE_SENSINF_OUT16','K1:PEM-EY1_SEIS_NS_SENSINF_OUT16','K1:PEM-EY1_SEIS_Z_SENSINF_OUT16']).T    
+    if True:
+        v = 5500.0
+        EX1_SEIS = EX1_SEIS/v
+        EY1_SEIS = EY1_SEIS/v
+        IY0_SEIS = IY0_SEIS/v    
+    if False:
+        Xend_NSEW = EX1_SEIS
+        Yend_NSEW = EY1_SEIS
+        Cent_NSEW = IY0_SEIS
+    if True:
+        # 3/28 -  , all 210 deg rotation to X arm
+        Xend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EX1_SEIS))
+        Yend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EY1_SEIS))
+        Cent_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),IY0_SEIS))        
+    if False:
+        # 3/12- 3/28 , Y:180,X:90,C:90.
+        Xend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EX1_SEIS))
+        Yend_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),EY1_SEIS))
+        Cent_NSEW = np.array(map(lambda x:np.dot(x,R(180.+theta)),IY0_SEIS))    
+    # Rotate EW axis to X-arm.
+    Xend_x, Xend_y, Xend_z = Xend_NSEW.T
+    Cent_x, Cent_y, Cent_z = Cent_NSEW.T
+    Yend_x, Yend_y, Yend_z = Yend_NSEW.T
+    time = np.arange(len(Xend_x))/fs   
+    # for plotting 3x3 plot
+    if True:
+        data = [[time,Xend_x],[time,Xend_y],[time,Xend_z],
+                [time,Cent_x],[time,Cent_y],[time,Cent_z],
+                [time,Yend_x],[time,Yend_y],[time,Yend_z]]    
+        label = ['Xend2F_x','Xend2F_y','Xend2F_z',
+                'Cent1F_x','Cent1F_y','Cent1F_z',
+                'Yend2F_x','Yend2F_y','Yend2F_z']
+        fname = '{0}/{1}_{2}_{3}.png'.format(title,start,tlen,option)
+        cmd = 'mkdir -p {0}'.format(title)
+        print cmd
+        ret  =  subprocess.check_call( cmd.split(" ") )
+        mpplot.subplot33(data,fname,label)
+    if True:
+        Yend_x = mptypes.Timeseries(Yend_x,fs=8,name='Yend_x')
+        Xend_x = mptypes.Timeseries(Xend_x,fs=8,name='Xend_x')
+        Cent_x = mptypes.Timeseries(Cent_x,fs=8,name='Cent_x')
+        Yend_x.get_psd(ave=8,plot=False,integ=True)
+        Xend_x.get_psd(ave=8,plot=False,integ=True)
+        Cent_x.get_psd(ave=8,plot=False,integ=True)
+        #
+        Yend_y = mptypes.Timeseries(Yend_y,fs=8,name='Yend_y')
+        Xend_y = mptypes.Timeseries(Xend_y,fs=8,name='Xend_y')
+        Cent_y = mptypes.Timeseries(Cent_y,fs=8,name='Cent_y')
+        Yend_y.get_psd(ave=8,plot=False,integ=False)
+        Xend_y.get_psd(ave=8,plot=False,integ=False)
+        Cent_y.get_psd(ave=8,plot=False,integ=False)
+        #
+        Yend_z = mptypes.Timeseries(Yend_z,fs=8,name='Yend_z')
+        Xend_z = mptypes.Timeseries(Xend_z,fs=8,name='Xend_z')
+        Cent_z = mptypes.Timeseries(Cent_z,fs=8,name='Cent_z')
+        Yend_z.get_psd(ave=8,plot=False,integ=False)
+        Xend_z.get_psd(ave=8,plot=False,integ=False)
+        Cent_z.get_psd(ave=8,plot=False,integ=False)
+        print 'get KAGRA data'
+        return [[Xend_x,Yend_x,Cent_x],[Xend_y,Yend_y,Cent_y],[Xend_z,Yend_z,Cent_z]]
 
 if __name__ == '__main__':
     start,tlen,title,option = main_getGPStime()
@@ -218,46 +216,58 @@ if __name__ == '__main__':
     Xend_y,Yend_y,Cent_y = data[1]
     Xend_z,Yend_z,Cent_z = data[2]    
     data = getGIFdata(start,tlen,title,option,theta=theta)
-    Strain,TR240_x,Baro_x500,Baro_x2000,Temp_x500,Temp_x2000 = data
-    plot_Strain=False
-    if plot_Strain:
+    Strain,TR240_x,Baro_x500,Baro_x2000,Temp_x500,Temp_x2000,CMRR = data
+    #
+    if False:
         data = [Strain,TR240_x,Baro_x500,Baro_x2000]#,Xend_x,Cent_x]
         mpplot.LogLogPlot(data,
-                        lim=(None,[0.5e-14,1e-10]),
-                        filename='./XarmStrain',
+                        lim=([1e-3,4e0],[0.5e-13,1e-9]),
+                        filename='{0}/XarmStrain_{1}_{2}_{3}'.format(title,start,tlen,option),
                         label=['Frequency [Hz]','Strain [1/rtHz]']
         )
-    plot_Seismometer=False
-    if plot_Seismometer:
+    if True:
+        data = [Strain,TR240_x,CMRR]
+        #data = [Xend_x]        
+        mpplot.LogLogPlot(data,
+                        lim=(None,[1e-3,1e3]),
+                        filename='{0}/XarmDisplace_{1}_{2}_{3}'.format(title,start,tlen,option),
+                        label=['Frequency [Hz]','Displacement [m/rtHz]']
+        )
+    if False:
         data = [TR240_x,Xend_x,Cent_x,Yend_x,Baro_x500]
         mpplot.LogLogPlot(data,
                         lim=(None,[0.5e-14,1e-10]),
-                        filename='./AllSeismometer',
+                        filename='{0}/AllSeismometer_{1}_{2}_{3}'.format(title,start,tlen,option),                        
                         label=['Frequency [Hz]','Strain [1/rtHz]']
         )        
-    coherence(TR240_x,Temp_x500)
-    coherence(TR240_x,Baro_x500)
-    coherence(TR240_x,Temp_x2000)
-    coherence(TR240_x,Baro_x2000)
-    coherence(TR240_x,Xend_x)
-    coherence(TR240_x,Cent_x)
-    coherence(TR240_x,Yend_x)
-    #
-    coherence(Strain,Temp_x500)
-    coherence(Strain,Baro_x500)
-    coherence(Strain,Temp_x2000)
-    coherence(Strain,Baro_x2000)
-    coherence(Strain,TR240_x)
-    coherence(Strain,Xend_x)
-    coherence(Strain,Cent_x)
-    coherence(Strain,Yend_x)
-    #
-    coherence(Xend_x,Yend_x)
-    coherence(Xend_x,Cent_x)
-    coherence(Yend_x,Cent_x)
-    coherence(Xend_y,Yend_y)
-    coherence(Xend_y,Cent_y)
-    coherence(Yend_y,Cent_y)
-    coherence(Xend_z,Yend_z)
-    coherence(Xend_z,Cent_z)
-    coherence(Yend_z,Cent_z)
+    if False:
+        for a in [Xend_x,Yend_x,Cent_x]:
+            for b in [Xend_x,Yend_x,Cent_x]:
+                a.get_coherence(b,8.0,plot=True)
+        for a in [Xend_y,Yend_y,Cent_y]:
+            for b in [Xend_y,Yend_y,Cent_y]:
+                a.get_coherence(b,8.0,plot=True)
+        for a in [Xend_z,Yend_z,Cent_z]:
+            for b in [Xend_z,Yend_z,Cent_z]:
+                a.get_coherence(b,8.0,plot=True)
+        for a in [Cent_x,Cent_x,Cent_x]:
+            for b in [Xend_x,Yend_x,Cent_x]:
+                a.get_coherence(b,8.0,plot=True)
+        for a in [Cent_x,Cent_x,Cent_x]:
+            for b in [Xend_y,Yend_y,Cent_y]:
+                a.get_coherence(b,8.0,plot=True)
+        for a in [Cent_x,Cent_x,Cent_x]:
+            for b in [Xend_z,Yend_z,Cent_z]:
+                a.get_coherence(b,8.0,plot=True)
+                
+        Strain.get_coherence(Xend_x,8.0,plot=True)
+        Strain.get_coherence(Yend_x,8.0,plot=True)
+        Strain.get_coherence(Cent_x,8.0,plot=True)
+        Strain.get_coherence(TR240_x,8.0,plot=True)
+        Strain.get_coherence(Baro_x500,8.0,plot=True)
+        Strain.get_coherence(Baro_x2000,8.0,plot=True)
+        TR240_x.get_coherence(Xend_x,8.0,plot=True)
+        TR240_x.get_coherence(Yend_x,8.0,plot=True)
+        TR240_x.get_coherence(Cent_x,8.0,plot=True)
+        TR240_x.get_coherence(Baro_x500,8.0,plot=True)
+        TR240_x.get_coherence(Baro_x2000,8.0,plot=True)
