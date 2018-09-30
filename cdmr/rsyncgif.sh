bc1a0caa036a15feb088e3811e5b89061601edc5
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-09-30 17:28:41 +0900

    first commit. make README.md

--- cdmr/rsyncgif.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rsync -av --include="*/" --include="*.STRAIN" --exclude="*" -e ssh GIF@172.16.32.201:/data1/PHASE/50000Hz/2018/08/27/* /Users/miyo/Dropbox/KagraData/gif/data1/PHASE/50000Hz/2018/08/27/
