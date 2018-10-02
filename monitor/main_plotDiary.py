9ca5de7912c98500930102e5d639277c7fd40411
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-02 17:19:24 +0900

    renew all monito

--- monitor/main_plotDiary.py
+++ monitor/main_plotDiary.py
@@ -1,17 +1,21 @@
 #
 #! coding:utf-8
-from plotTimeseries import plotBandPassTimeseries
+#from plotTimeseries import plotBandPassTimeseries
 from miyopy.io import read
+from miyopy.io.read import save_to_dump,DumpFileException
+from miyopy.plot import plotBandPassTimeseries
+
 
 def main(start,chname):
     if start%(3600*24)==54018: # 00h00m00sの条件
         end = start + 3600*24
-        data = read(start,end,chname,fmt='nds')
-        plotBandPassTimeseries(data)
-        
-        #plotBandPassTimeseries(start,start+24*3600,
-        #                       chname,
-        #                       imgdir='./daily/{0}/'.format(start))
+        try:
+            value = read(start,end,chname,fmt='dump')
+        except DumpFileException as e:
+            data = read(start,end,[chname],fmt='nds')
+            value = data[0].data            
+            save_to_dump(value,start,end,chname)
+        plotBandPassTimeseries(value,start,end,chname,imgdir='./{0}/'.format(start))
     else:
         raise ValueError('start time dose not match "00h00m00s". start:{}' \
                          .format(start))
