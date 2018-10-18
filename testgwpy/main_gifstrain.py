93e884d00b0ea65f5f355e3d5c5a6bd3728446ba
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-18 21:06:59 +0900

    add testgwpy png files

--- testgwpy/main_gifstrain.py
+++ testgwpy/main_gifstrain.py
@@ -2,42 +2,56 @@
 #! coding:utf-8
 import sys
 sys.path.insert(0,'/Users/miyo/Dropbox/Git/gwpy/')
+sys.path.insert(0,'/Users/miyo/Dropbox/Git/miyopy/')
 
+from numpy import (array)
 from gwpy.timeseries import TimeSeries
 import gwpy
 
+from miyopy.gif import (GifData)
 
 if __name__=='__main__':
-    strain = TimeSeries.read(source='./1810140000.STRAIN',
+    prefix = '/Users/miyo/Dropbox/KagraData/gif/data1/PHASE/50000Hz/2018/10/14/00/'
+
+    start = 1223478018
+    start = 1222441218
+    start = 1222786818 # 10/06 00:00:00 JST
+    tlen = 3600*24*3
+    chname = 'CALC_STRAIN'
+    files = GifData.findfiles(start,tlen,chname,
+                              prefix='/Users/miyo/Dropbox/KagraData/gif/')
+    print('Found files')
+    
+    strain = TimeSeries.read(source=files,
                              name='CALC_STRAIN',
-                             start=1223478018,
-                             format='gif')
-
-
+                             format='gif',
+                             nproc=2)
+    
     # plot timeseries
     plot = strain.plot(title='GIF strain data',
                        ylabel='Strain amplitude',
                        )
     plot.savefig('timeseries.png')
     plot.close()
-
+    print('plot timeseries.png')
     
     # plot spectrogram
-    stride = 10 # sec
-    fftlength = 5 # sec
+    stride = 2**12 # sec
+    fftlength = 2**11 # sec
     spectrogram = strain.spectrogram(stride=stride, fftlength=fftlength) ** (1/2.)
     plot = spectrogram.imshow(norm='log', vmin=5e-12, vmax=1e-10)
     ax = plot.gca()
     ax.set_yscale('log')
-    ax.set_ylim(0.01, 200)
+    ax.set_ylim(1e-3, 10)
     ax.colorbar(
         label=r'GIF strain amplitude [strain/$\sqrt{\mathrm{Hz}}$]')
+    plot.subplots_adjust(right=.86)
     plot.savefig('spectrogram.png')
     plot.close()
-
+    print('plot spectrogram.png')
     
     # plot asd
-    fftlength = 5
+    fftlength = 2**11
     overlap = 2
     sg = strain.spectrogram2(fftlength=fftlength,
                              overlap=overlap, window='hanning') ** (1/2.)
@@ -46,7 +60,7 @@ if __name__=='__main__':
     high = sg.percentile(95)
     from gwpy.plot import Plot
     plot = Plot()
-    ax = plot.gca(xscale='log', xlim=(0.01, 200), xlabel='Frequency [Hz]',
+    ax = plot.gca(xscale='log', xlim=(1e-7, 10), xlabel='Frequency [Hz]',
                 yscale='log', ylim=(5e-14, 1e-5),
                 ylabel=r'Strain noise [1/\rtHz]')
     ax.plot_mmm(median, low, high, color='gwpy:ligo-hanford')
@@ -54,3 +68,4 @@ if __name__=='__main__':
                 fontsize=16)
     plot.savefig('asd.png')
     plot.close()
+    print('plot asd.png')
