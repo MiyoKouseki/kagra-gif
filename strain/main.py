4d280bc3caff9852b803b4fabe2ecb235928faab
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-21 16:33:59 +0900

    Add strain

---
+++ strain/main.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.insert(0,'/Users/miyo/Dropbox/Kagra/Git/gwpy/')
+sys.path.insert(0,'/Users/miyo/Dropbox/Git/miyopy/')
+
+import numpy
+from astropy import units as u 
+import matplotlib.pyplot as plt
+
+from gwpy.timeseries import TimeSeries
+
+from miyopy.gif import (GifData,KagraStrain,gps2datestr)
+from gwpy.plot import Plot
+
+start = 1223996418-3600*24*4
+tlen = 3600*24*5
+end = start + tlen
+#
+# -- kagra gif x -----------------------
+segments = GifData.findfiles(start,tlen,'CALC_STRAIN',
+                             prefix='/Users/miyo/Dropbox/KagraData/gif/')
+allfiles = [path for files in segments for path in files]
+strain = TimeSeries.read(source=allfiles,
+                         name='CALC_STRAIN',
+                         format='gif',
+                         pad=numpy.nan,
+                         nproc=2)
+strain = strain.detrend('linear')
+#
+# -- gotic -------------
+gifx = KagraStrain(start,end).x
+gifx = gifx.detrend('linear')
+gifx = gifx*0.9
+#
+# -- plot  -----------------------
+print(gifx)
+print(strain)
+#plot = gifx.plot()
+plot = Plot(gifx,strain,xscale='auto-gps')
+#plot.auto_gps_label()
+#plot.legend()
+plot.subplots_adjust(right=.86)
+plot.savefig('x_.png')
+plot.close()
