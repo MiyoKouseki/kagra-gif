a7ceb49ef1d4f39a76afa2355a8efd45290d4e92
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-23 17:39:03 +0900

    Move unused function in main_selfnoise to main_Trillium

--- selfnoise/main_selfnoise.py
+++ selfnoise/main_selfnoise.py
@@ -40,115 +40,6 @@ channels = ['K1:PEM-IXV_SEIS_NS_SENSINF_INMON.mean',
             'K1:PEM-IXV_SEIS_TEST_Z_SENSINF_INMON.mean']
 
 
-def coh2snr(coh):
-    '''
-    
-    '''
-    _coh2snr = lambda x:  abs(x)/(1.0-abs(x))
-    snr = [_coh2snr(_coh) for _coh in coh]
-    return np.array(snr)
-
-
-
-def sensor_noise(coh12,coh13,asd1,asd2,asd3):
-    ''' Estimate the noise level of sensor using Peterson-Method(1980).
-    
-            
-    Parameters
-    ----------
-    coh12 : numpy.array 
-        The coherence between "adjacent two sensors" so that the 
-        external noise from local environment is same each other.
-    coh13 : numpy.array
-        The coherence between "well-separate two sensors" so that the
-        external noise from local environment dont have correlation
-        earch other.        
-    asd1 : numpy.array
-       Amplitude spectrum density of the sensor1.
-    asd2 : numpy.array
-       Amplitude spectrum density of the sensor2.
-    asd3 : numpy.array
-       Amplitude spectrum density of the sensor3.
-    
-    Returns
-    -------
-    asd_x : numpy.array
-        The noise existing globally with coherently. This noise could
-        be called "signal", because this noise have coherent globally.
-    asd_n : numpy.array
-        The noise existing globally with incoherently. This noise could 
-        be called "self-noise", because this incoherenct noise dont 
-        disappear even separated well.
-    asd_xb : numpy.array
-        The noise existing locally with coherently. This noise is a 
-        sum of the signal and local environmental noise.
-    '''
-    coh13 = np.sqrt(coh13)
-    coh12 = np.sqrt(coh12)
-    snr13 = coh2snr(coh13)
-    snr12 = coh2snr(coh12)
-    n12 = asd1*np.sqrt(1.0/(1+snr12))
-    s12 = asd1*np.sqrt(1.0/(1+snr12)*snr12)
-    s13 = np.sqrt(asd1*asd3*coh13)
-    envnoise = s12 - s13
-    asd_n = n12
-    asd_x = s13
-    asd_xb = s12
-    return asd_x,asd_n,asd_xb
-
-
-
-
-def huge(t1,t2,t3,start,end,fs,ave=32):
-    t1_we,t1_ns,t1_zz = t1
-    t2_we,t2_ns,t2_zz = t2
-    t3_we,t3_ns,t3_zz = t3
-    f_, asd1_we = asd(t1_we,fs,ave=ave)
-    f_, asd2_we = asd(t2_we,fs,ave=ave)
-    f_, asd3_we = asd(t3_we,fs,ave=ave)    
-    asd1_we = asd1_we/H120QA(f_)
-    asd2_we = asd2_we/H120QA(f_)
-    asd3_we = asd3_we/H120QA(f_)    
-    asdplot([[f_,asd1_we],[f_,asd2_we],[f_,asd3_we]],
-            legend=['1_we','2_we','3_we'],
-            fname='asd')
-
-    
-def plot_asd_all(t1,t2,t3,start,end,fs,ave=32):
-    #
-    t1_we,t1_ns,t1_zz = t1
-    t2_we,t2_ns,t2_zz = t2
-    t3_we,t3_ns,t3_zz = t3
-    f_, asd1_we = asd(t1_we,fs,ave=ave)
-    f_, asd2_we = asd(t2_we,fs,ave=ave)
-    f_, asd3_we = asd(t3_we,fs,ave=ave)    
-    asd1_we = asd1_we/H120QA(f_)
-    asd2_we = asd2_we/H120QA(f_)
-    asd3_we = asd3_we/H120QA(f_)
-    f, coh13_wewe, deg13_wewe = coherence(t1_we,t3_we,fs,ave=ave)
-    f, coh12_wewe, deg12_wewe = coherence(t1_we,t2_we,fs,ave=ave)
-    #
-    asd_x,asd_n,asd_xb = sensor_noise(coh12_wewe,coh13_wewe,asd1_we,asd2_we,asd3_we)
-    #
-    snr13_wewe = coh2snr(coh13_wewe)
-    snr12_wewe = coh2snr(coh12_wewe)
-    asdplot([[f_,snr12_wewe],[f_,snr13_wewe]],
-            legend=['1_we','2_we'],
-            fname='snr')
-    #
-    f,_selfnoise = TRselfnoise(acc='velo')    
-    asdplot([[f,_selfnoise],[f_,asd_x],[f_,asd_n],[f_,asd_xb]],
-            legend=['Self-Noise',
-                    '1_we',
-                    'x : Coherent Signal b/w 1 and 3 (Global Noise)',
-                    'n : Incoherent Signal b/w 1 and 2 (Internal Instrument Noise)',
-                    'x+b : Coherent Signal b/w 1 and 2 (Local Noise)',       
-                    ],
-            linestyle=[':o','-','-','-'],
-            ylim=[1e-11,5e-6],
-            xlim=[min(f_[1:]),200],
-            fname='selfnoise')    
-
    
 def dump(chname,start,tlen):    
     end = start + tlen
@@ -180,16 +71,40 @@ def main(channel,start,end):
     data = TimeSeries.read(
         dumped_gwf_fmt.format(start=start,end=end,chname=channel),
         channel, verbose=True ,nproc=8)
-    
+
     timeseriesplot_fname_fmt = 'TimeSeries_{channel}.png'
     spectrogramplot_fname_fmt = 'Spectrogram_{channel}.png'
     asdplot_fname_fmt = 'ASD_{channel}.png'
 
     # Filtering
     from gwpy.signal import filter_design
-    bp_high = filter_design.highpass(0.3, data.sample_rate)
-    bp_mid = filter_design.bandpass(0.05, 0.3, data.sample_rate)
-    bp_low = filter_design.lowpass(0.05, data.sample_rate)
+    from gwpy.plot import BodePlot
+    import numpy
+    bp_high = filter_design.highpass(0.3, data.sample_rate, analog=True,
+                                     ftype='butter',
+                                     gpass=2,gstop=30#,fstop()
+    )
+    bp_mid = filter_design.bandpass(0.05, 0.3, data.sample_rate, analog=True,
+                                    ftype='butter',
+                                    gpass=2, gstop=30#,fstop=(0.01,0.5)
+    )
+    bp_low = filter_design.lowpass(0.05, data.sample_rate,analog=True, 
+                                   ftype='butter',
+                                   gpass=2, gstop=30#, fstop=2
+    )
+    filters = [bp_high,bp_mid,bp_low]
+
+    plot = BodePlot(*filters, analog=True,
+                    frequencies=numpy.logspace(-3,1,1e5),
+                    dB=False,unwrap=False,
+                    title='filter')
+    axes = plot.get_axes()
+    axes[0].set_yscale('log')
+    axes[0].set_ylim(1e-4,2e0)
+    axes[-1].set_xlim(1e-2,1e0)
+    axes[-1].set_ylim(-180,180)
+    plot.savefig('Bodeplot_BandPass.png')
+    plot.close()
 
     data_high = data.filter(bp_high, filtfilt=True)
     data_high = data_high.crop(*data_high.span.contract(1))
