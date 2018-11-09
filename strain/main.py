0651def05cc32c32df3307e8677bdbb022cd65e2
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-11-09 14:55:01 +0900

    Use gpsdatestr

--- strain/main.py
+++ strain/main.py
@@ -9,22 +9,24 @@ import matplotlib.pyplot as plt
 
 from gwpy.timeseries import TimeSeries
 
-from miyopy.gif import (GifData,KagraStrain)#,gps2datestr)
+from miyopy.gif import (GifData,KagraStrain,gps2datestr)
 from gwpy.plot import Plot
 
 start = 1223996418-3600*24*4
 tlen = 3600*24*5
 end = start + tlen
-#
+
 # -- kagra gif x -----------------------
 segments = GifData.findfiles(start,tlen,'CALC_STRAIN',
                              prefix='/Users/miyo/Dropbox/KagraData/gif/')
+
 allfiles = [path for files in segments for path in files]
 strain = TimeSeries.read(source=allfiles,
                          name='CALC_STRAIN',
                          format='gif',
                          pad=numpy.nan,
                          nproc=2)
+#
 strain = strain.detrend('linear')
 #
 # -- gotic -------------
