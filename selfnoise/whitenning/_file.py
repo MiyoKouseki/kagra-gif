52181bcde14b5b0b265b07eeea188d4019bb9d0a
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-11-20 15:38:47 +0900

    Backup

---
+++ selfnoise/whitenning/_file.py
@@ -0,0 +1,117 @@
+import os 
+import re
+import warnings
+import numpy as np
+
+from gwpy.frequencyseries import FrequencySeries
+from gwpy.timeseries import TimeSeries
+from gwpy.spectrogram import Spectrogram
+
+def get_csd_specgram(chname1,chname2,remake=True,fftlength=2**6,**kwargs):
+    hdf5fname = to_hdf5fname(chname1,chname2)
+    if remake:
+        if os.path.exists(hdf5fname):
+            os.remove(hdf5fname)
+        timeseries1 = get_timeseries(chname1,from_nds=False,**kwargs)
+        timeseries2 = get_timeseries(chname2,from_nds=False,**kwargs)
+        specgram = timeseries1.csd_spectrogram(timeseries2,
+                                               stride=fftlength,
+                                               fftlength=fftlength,
+                                               overlap=0,
+                                               window='hanning',
+                                               nproc=2)
+        specgram.write(hdf5fname)
+        return specgram
+    else:
+        warnings.warn('Dont use fftlength option..')
+        specgram = Spectrogram.read(hdf5fname)
+        return specgram
+
+
+def get_specgram(chname,remake=False,fftlength=2**6,**kwargs):
+    hdf5fname = to_hdf5fname(chname)
+
+    if remake:
+        if os.path.exists(hdf5fname):
+            os.remove(hdf5fname)        
+        timeseries = get_timeseries(chname,from_nds=False,**kwargs)
+        specgram = timeseries.spectrogram(stride=fftlength,
+                                           fftlength=fftlength,
+                                           overlap=0,
+                                           window='hanning') ** (1/2.)            
+        specgram.write(hdf5fname)
+        return specgram
+    else:
+        warnings.warn('Dont use fftlength option..')
+        specgram = Spectrogram.read(hdf5fname)
+        return specgram
+
+
+
+
+def get_timeseries(chname,prefix='./',from_nds=False,**kwargs):    
+    fname = to_gwffname(chname)
+    start = kwargs.pop('start')
+    end = kwargs.pop('end')    
+    if not from_nds and os.path.exists(fname):
+        print('Skip fetch from nds {0}'.format(fname))
+        data = TimeSeries.read(fname,chname,start,end,
+                                format='gwf.lalframe',
+                                verbose=True,
+                                pad=np.nan)
+        
+        return data
+    else:
+        data = TimeSeries.fetch(chname, start, end,
+                                verbose=True,
+                                host='10.68.10.121', port=8088,
+                                pad=np.nan,
+                                verify=True,type=1,dtype=np.float32)
+        data.write(fname,format='gwf.lalframe')
+        print('wrote data in '+fname)
+        return data
+
+
+
+
+
+
+
+def to_gwffname(chname,prefix='./'):
+    print(chname)
+    m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname)
+    try:
+        fname = prefix + m.group(1).lower() + '.gwf'
+    except Exception as e:
+        print e
+        exit()
+    return fname
+
+
+def to_hdf5fname(*args,**kwargs):
+    prefix = kwargs.pop('prefix','./')
+    N = len(args)
+    if N==2:
+        chname1,chname2 = args
+        m1 = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname1)
+        m2 = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname2)
+        fname = prefix + m1.group(1).lower() + '-' + m2.group(1).lower() + '.hdf5'
+    elif N==1:
+        chname = args[0]
+        m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname)
+        try:
+            fname = prefix + m.group(1).lower() + '.hdf5'
+        except Exception as e:
+            print e
+    return fname
+    
+
+
+def to_pngfname(chname,ftype,prefix='./'):
+    m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname)
+    try:
+        fname = prefix + ftype + '_' + m.group(1).lower() + '.png'
+    except Exception as e:
+        print e
+        exit()
+    return fname
--- selfnoise/whitenning/main.py
+++ selfnoise/whitenning/main.py
@@ -1,41 +1,57 @@
-from dttdata import DttData
-
-dtt = DttData('xarm_seismometer_IN1_DQ.xml')
-ch1 = 'K1:PEM-IXV_SEIS_NS_SENSINF_IN1_DQ'
-ch2 = 'K1:PEM-IXV_SEIS_TEST_NS_SENSINF_IN1_DQ'
-ch3 = 'K1:PEM-EXV_SEIS_NS_SENSINF_IN1_DQ'
-
-if False:    
-    f,asd = dtt.getASD(ch1,ref=False)
-    f,mag,deg = dtt.getCoherence(ch1,ch2,ref=False)
-    
-    import matplotlib.pyplot as plt
-    fig, (ax0,ax1) = plt.subplots(2,1,figsize=(12, 6), dpi=80)
-    ax0.semilogx(f,mag,label='/'.join([ch2,ch2]))
-    ax0.legend()
-    ax0.set_ylim(0,1)
-    ax0.set_ylabel('Coherence',fontsize=20)
-    ax1.semilogx(f,deg,label='/'.join([ch2,ch2]))
-    ax1.legend()
-    ax1.set_ylim(-180,180)
-    ax1.set_yticks(range(-180,181,90))
-    ax1.set_ylabel('Phase [deg]',fontsize=20)
-    ax1.set_xlabel('Frequency [Hz]',fontsize=20)
-    plt.savefig('test_coh_.png')
-    plt.close()
+import sys
+sys.path.insert(0,'/Users/miyo/Dropbox/Git/gwpy/')
+import gwpy
+print gwpy.__file__
 
 from gwpy.frequencyseries import FrequencySeries
-from gwpy.plotter import Plot
 
-coh_12 = dtt.getCoherence(ch1,ch2,ref=False)
-f,mag,deg = coh_12
-mag_spec = FrequencySeries(mag, f0=f[0], df=f[1]-f[0])
-deg_spec = FrequencySeries(deg, f0=f[0], df=f[1]-f[0])
+from gwpy.timeseries import TimeSeries
+from gwpy.spectrogram import Spectrogram
 
-plot = mag_spec.plot(label='Un-cleaned')
-fig = plot.figure
-print fig
-#ax = plot.gca()
-#ax.plot(deg_spec, label='Cleaned')
-#plot = mag_spec.plot(ylabel='Magnitude',title='Coherence')
-plot.savefig('test_coh.png')
+from gwpy.time import tconvert
+from gwpy.plot import Plot
+
+from _file import (get_timeseries,get_specgram,get_csd_specgram,
+                   to_gwffname,to_pngfname,to_hdf5fname)
+
+from _plot import (plot_asd,plot_coherence,plot_spectrogram)                
+
+def plot_coherence_with_scipy(data1,data2,fftlength=2**7,**kwargs):
+    print data1    
+    
+
+
+if __name__ == '__main__':
+    fmt = 'K1:PEM-{seismometer}_{dof}_SENSINF_IN1_DQ'
+    
+    start = tconvert('Nov 12 3:0:0') # work finish at 12:00 JST
+    # Lack of Data!! 2018-11-12T12:48:14  --- 2018-11-12T12:50:54
+    #start = tconvert('Nov 12 14:30:0') # after daq restart
+    #start = tconvert('Nov 13 00:30:0') # after lack of data
+    start = tconvert('Nov 13 01:00:0') #
+    #end = tconvert('Nov 14 0:0:0') #
+    fftlength = 2**7
+    end = start + fftlength*2**4
+    
+    chname1 = 'K1:PEM-IXV_SEIS_NS_SENSINF_IN1_DQ'
+    chname2 = 'K1:PEM-EXV_SEIS_NS_SENSINF_IN1_DQ'
+    chname2 = 'K1:PEM-IXV_SEIS_TEST_NS_SENSINF_IN1_DQ'
+    
+    kwargs = {}
+    kwargs['start'] = start
+    kwargs['end'] = end
+
+    timeseries1 = get_timeseries(chname1,remake=False,fftlength=2**6,**kwargs)
+    timeseries2 = get_timeseries(chname2,remake=False,fftlength=2**6,**kwargs)    
+    specgram1 = get_specgram(chname1,remake=False,fftlength=2**6,**kwargs)
+    specgram2 = get_specgram(chname2,remake=False,fftlength=2**6,**kwargs)
+    cds_specgram = get_csd_specgram(chname1,chname2,remake=False,
+                                    fftlength=2**6,**kwargs)
+    
+    plot_asd(specgram1,replot=False,**kwargs)
+    plot_asd(specgram2,replot=False,**kwargs)
+    plot_spectrogram(specgram1,replot=False,**kwargs)
+    plot_spectrogram(specgram2,replot=False,**kwargs)
+    #plot_coherence(cds_specgram,specgram1,specgram2,fftlength=2**7,**kwargs)
+    #plot_coherence(chname1,chname2,fftlength=2**7,**kwargs)    
+    plot_coherence_with_scipy(timeseries1,timeseries2,fftlength=2**7,**kwargs)
