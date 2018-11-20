ce177aeb6f8889b5dc33232cb7eafe2c90ebf10c
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-11-20 18:03:21 +0900

    Add test function. It work well!

--- selfnoise/whitenning/main_gwpy.py
+++
@@ -1,8 +0,0 @@
-
-from gwpy.timeseries import TimeSeries
-
-chname = 'K1:PEM-IXV_SEIS_NS_SENSINF_IN1_DQ'
-start = 'Nov 12 3:0:0'
-end = 'Nov 12 4:0:0'
-data = TimeSeries.fetch(chname, start, end, host='10.68.10.121', port=8088)
-data.write('ixv_ns.gwf',format='gwf.lalframe')
