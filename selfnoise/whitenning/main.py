04ec28509af49247b25f6f707a6188f26b5acf47
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-11-21 07:09:32 +0900

    Update main

--- selfnoise/whitenning/main.py
+++ selfnoise/whitenning/main.py
@@ -18,11 +18,6 @@ from _file import (get_timeseries,get_specgram,get_csd_specgram,
 
 from _plot import (plot_asd,plot_coherence,plot_spectrogram)                
 
-def plot_coherence_with_scipy(data1,data2,fftlength=2**7,**kwargs):
-    data1
-    # ちゃんとASDとCSDが計算できているかテストプログラムを書いてから
-    #ここにくる。
-    exit()
 
 
 if __name__ == '__main__':
@@ -35,31 +30,39 @@ if __name__ == '__main__':
     start = tconvert('Nov 13 01:00:0') #
     #end = tconvert('Nov 14 0:0:0') #
     fftlength = 2**7
-    end = start + fftlength*2**4
+    end = start + fftlength*2**10
     
-    chname1 = 'K1:PEM-IXV_SEIS_NS_SENSINF_IN1_DQ'
-    chname2 = 'K1:PEM-EXV_SEIS_NS_SENSINF_IN1_DQ'
-    chname2 = 'K1:PEM-IXV_SEIS_TEST_NS_SENSINF_IN1_DQ'
+    chname1 = 'K1:PEM-IXV_SEIS_WE_SENSINF_IN1_DQ'
+    chname2 = 'K1:PEM-EXV_SEIS_WE_SENSINF_IN1_DQ'
+    chname2 = 'K1:PEM-IXV_SEIS_TEST_WE_SENSINF_IN1_DQ'
     
     kwargs = {}
     kwargs['start'] = start
     kwargs['end'] = end
+    kwargs['nds'] = True
+    
+    timeseries1 = get_timeseries(chname1,remake=True,fftlength=fftlength,**kwargs)
+    timeseries2 = get_timeseries(chname2,remake=True,fftlength=fftlength,**kwargs)    
+    specgram1 = get_specgram(chname1,remake=True,fftlength=fftlength,**kwargs)
+    specgram2 = get_specgram(chname2,remake=True,fftlength=fftlength,**kwargs)
+    csd_specgram = get_csd_specgram(chname1,chname2,remake=True,
+                                    fftlength=fftlength,**kwargs)
+    coherence2_mag_specgram = csd_specgram.abs()#**2.0/specgram1/specgram2
+    coherence2_angle_specgram = csd_specgram.angle()
+    coherence_mag_specgram = coherence2_mag_specgram#**(1/2.0)
+    #print coherence_mag_specgram#.mean(axis=0)
 
-    timeseries1 = get_timeseries(chname1,remake=True,fftlength=2**6,**kwargs)
-    timeseries2 = get_timeseries(chname2,remake=True,fftlength=2**6,**kwargs)    
-    specgram1 = get_specgram(chname1,remake=True,fftlength=2**6,**kwargs)
-    specgram2 = get_specgram(chname2,remake=True,fftlength=2**6,**kwargs)
-    cds_specgram = get_csd_specgram(chname1,chname2,remake=True,
-                                    fftlength=2**6,**kwargs)
-
-    print specgram1.unit
-    print specgram1.unit
-    print cds_specgram.unit
+    coherencegram  = timeseries1.coherence_spectrogram(timeseries2,
+                                                       stride=fftlength*2.0,
+                                                       fftlength=fftlength,
+                                                       overlap=0)
+    coherence_mag_specgram = coherencegram
+    print coherence_mag_specgram
+    
     plot_asd(specgram1,replot=True,**kwargs)
     plot_asd(specgram2,replot=True,**kwargs)
     plot_spectrogram(specgram1,replot=True,**kwargs)
     plot_spectrogram(specgram2,replot=True,**kwargs)
-    plot_coherence(cds_specgram,specgram1,specgram2,fftlength=2**7,**kwargs)
-    #plot_coherence(chname1,chname2,fftlength=2**7,**kwargs)    
-    #plot_coherence_with_scipy(timeseries1,timeseries2,fftlength=2**7,**kwargs)
+    plot_spectrogram(coherence_mag_specgram,replot=True,normlog=False,**kwargs)
+    plot_coherence(csd_specgram,specgram1,specgram2,fftlength=fftlength,**kwargs)
     
