a1404bc20123f55de94714d64f9cd96737ec0d40
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-12-10 14:14:21 +0900

    Rename to weatherstation

--- airEnvironmentMonitor/measurementA/rsync_m31_kagragif_data.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-rsync -P -av -e ssh m31-01:/home/kouseki.miyo/kagra-gif/airEnvironmentMonitor/measurementA/*.gwf ./
--- airEnvironmentMonitor/test_gwpynds2.py
+++
@@ -1,5 +0,0 @@
-from gwpy.detector import ChannelList
-chname = 'K1:PEM-IXV_GND*'
-chlst = ChannelList.query_nds2([chname],host='10.68.10.121',port=8088)
-print chlst
-
