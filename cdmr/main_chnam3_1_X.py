2a058277d63a11c1207c3d2f0483b8b53a5dcf5f
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-01-30 19:14:58 +0900

    Add make_chname3_hdf5.py

--- cdmr/main_chnam3_1_X.py
+++ cdmr/main_chnam3_1_X.py
@@ -32,67 +32,37 @@ chname3_1_X.gwf data contains only X axis.
 
 '''
 
-def calc_asd(ts, fftlength=2**10):
-    sg = ts.spectrogram2(fftlength=fftlength, overlap=fftlength/2., window='hanning') ** (1/2.)
-    median_0,low_0,high_0 = sg.percentile(50), sg.percentile(5), sg.percentile(95)
-    return median_0,low_0,high_0
+# gif data
+pfx = '/Users/miyo/KagraData/gif/'
+from miyopy.gif.datatype import GifData
+start = 'Dec 02 2018 11:00:00'
+end = 'Dec 03 2018 07:00:00'
 
-# Data taking
-start0 = tconvert('Dec 02 2018 11:00:00 JST')
-end0 = tconvert('Dec 03 2018 07:00:00 JST')
-end0 = start0 + 2**16
-start,end = start0,end0
-chname = ['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ',
-          'K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ',
-          'K1:PEM-EXV_GND_TR120Q_X_IN1_DQ',
-]
-kwargs = {}
-kwargs['verbose'] = True
-kwargs['format'] = 'gwf.lalframe'
-kwargs['start'] = start
-kwargs['end'] = end
-kwargs['pad'] = np.nan
-kwargs['nproc'] = 5
-data = TimeSeriesDict.read('chname3_1_X.gwf',chname,**kwargs)
+segments = GifData.findfiles(start,end,'X1500_TR240velNS',prefix=pfx)
+allfiles = [path for files in segments for path in files]
+strain = TimeSeries.read(source=allfiles,
+                         name='CALC_STRAIN',
+                         format='gif',
+                         pad=np.nan,
+                         nproc=2)
+strain = strain.detrend('linear')
+
+exit()
+
+def read(fname):
+    return FrequencySeries.read(fname,format='hdf5')        
+median_0 = read('chname3_1_X_exv_50pct.hdf5')
+median_1 = read('chname3_1_X_ixv1_50pct.hdf5')
+median_2 = read('chname3_1_X_ixv2_50pct.hdf5')
+median_d12 = read('chname3_1_X_diff12_50pct.hdf5')
+median_c12 = read('chname3_1_X_comm12_50pct.hdf5')
 
 
-# Override unit of data from count to voltage.
-c2v = (10/2**15)*u.V/u.ct # [1]
-# [1] In the case of differential output.
-exv_x = data['K1:PEM-EXV_GND_TR120Q_X_IN1_DQ']#.override_unit('ct')#*c2v
-ixv1_x = data['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ']#.override_unit('ct')#*c2v
-ixv2_x = data['K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ']#.override_unit('ct')#*c2v
-exv_x.override_unit('ct')#*c2v
-ixv1_x.override_unit('ct')#*c2v
-ixv2_x.override_unit('ct')#*c2v
-exv_x = exv_x*c2v
-ixv1_x = ixv1_x*c2v
-ixv2_x = ixv2_x*c2v
 
-# Calc differential and common components.
-diff12 = ixv1_x - ixv2_x
-diff02 = exv_x - ixv2_x
-comm12 = ixv1_x - ixv2_x
-comm02 = exv_x - ixv2_x
-
-# Calc asd
-median_0,low_0,high_0 = calc_asd(exv_x)
-median_1,low_1,high_1 = calc_asd(ixv1_x)
-median_2,low_2,high_2 = calc_asd(ixv2_x)
-median_d12,low_d12,high_d12 = calc_asd(diff12)
-median_d01,low_d01,high_d01 = calc_asd(diff02)
-
-# Calibrate to velocity.    
-amp = 10**(30/20.)
-_v2vel = 1/1202.5*u.m/u.s/u.V
-v2vel = Trillium120q.v2vel
-median_0,low_0,high_0 = v2vel(median_0)/amp, v2vel(low_0)/amp, v2vel(high_0)/amp
-median_1,low_1,high_1 = v2vel(median_1)/amp, v2vel(low_1)/amp, v2vel(high_1)/amp
-median_2,low_2,high_2 = v2vel(median_2)/amp, v2vel(low_2)/amp, v2vel(high_2)/amp
-median_d12,low_d12,high_d12 = v2vel(median_d12)/amp, v2vel(low_d12)/amp, v2vel(high_d12)/amp
-median_d01,low_d01,high_d01 = v2vel(median_d01)/amp, v2vel(low_d01)/amp, v2vel(high_d01)/amp
 
 # Get several noise
+amp = 10**(30/20.)
+v2vel = Trillium120q.v2vel
 _adcnoise = 2e-6*u.V # Equivalent Input Noise [V/rtHz]. [1] 
 _ampnoise = 6e-7*u.V # Output Noise [V/rtHz]. [2]
 # [1] JGW-S1402729 (for IY1 not found IX1..)
@@ -114,6 +84,7 @@ ax = plot.gca(xscale='log', xlim=(1e-3, 100), xlabel='Frequency [Hz]',
               yscale='log', ylim=(1e-11, 1e-5),
               ylabel=r'Velocity [m/sec/\rtHz]')
 
+
 if True:
     ax.loglog(selfnoise,label='Self Noise',linewidth=2,alpha=0.7,linestyle='--')
     ax.loglog(adcnoise,label='ADC EIN',linewidth=2,alpha=0.7,linestyle='--')
@@ -123,6 +94,7 @@ if True:
     ax.loglog(median_2,label='IXV2')
     ax.loglog(median_d12,label='IXV1-IXV2')
     #ax.loglog(median_d01,label='diff01')
+
     
 ax.legend()    
 ax.set_title('Seismometer',fontsize=16)
