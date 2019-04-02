01d7de27eceabfb3c1f7e3d770b1e1157ad5955a
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-04-02 16:37:14 +0900

    Update michelson

--- finesse/mich_tf.py
+++ finesse/mich_tf.py
@@ -1,7 +1,6 @@
 #
 # Reference 
 #  [1] https://gwdoc.icrr.u-tokyo.ac.jp/cgi-bin/DocDB/ShowDocument?docid=1669
-import numpy as np
 from pykat import finesse
 import matplotlib.pyplot as plt
 
@@ -21,23 +20,22 @@ s east-s 3001 n3 n6
 m north-m 0.99995 50e-6 0 n5 n7
 m east-m 0.99995 50e-6 90 n6 n8
 pd1 Ctrl 10M 90 nAS
-xaxis north-m phi lin -180 180 400
-func y = 90-$x1
-put* east-m phi $y # 
-noplot y
-yaxis lin abs
+fsig sig1 east-m 1000 0
+fsig sig2 north-m 1000 180
+pd2 GW 10M 0 1000 max nAS
+xaxis sig1 f log 1 10k 100
+put GW f2 $x1
+scale meter
+yaxis log abs
 '''
 kat.parse(kattext)
 out = kat.run()
 deg = out.x
-asPD = out.y[:,0]
+asPD = out.y[:,1]
 fig, ax = plt.subplots(1, 1, figsize=(10,6), dpi=320)
-plt.plot(deg,asPD)
-plt.xticks(np.arange(-90,91,45))
-plt.xlim(-90,90)
-plt.title('')
-plt.legend(['AS (Q)'])
-plt.xlabel('tuning DARM [deg]')
-plt.ylabel('Abs')
-plt.grid(b=None, which='both', axis='both', linestyle='--')
-plt.savefig('mich.png')
+plt.loglog(deg,asPD)
+#plt.plot(deg,asPD)
+plt.title(r'Phase difference at the ITM')
+plt.xlabel('tuning ETM [deg]')
+plt.ylabel('phase difference [deg]')
+plt.savefig('mich_tf.png')
