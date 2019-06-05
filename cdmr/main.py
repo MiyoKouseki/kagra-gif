4db8a7b910ef55eac345a9d1b5659956e97289f2
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-06-05 11:42:01 +0900

    Update

--- cdmr/main.py
+++ cdmr/main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0,'../../miyopy')
 
 import numpy as np
+from scipy.special import jv
 import matplotlib
 matplotlib.use('Agg')
 
@@ -11,11 +12,11 @@ from gwpy.plot import Plot
 from gwpy.timeseries import TimeSeriesDict,TimeSeries
 from gwpy.frequencyseries import FrequencySeries
 from gwpy.time import tconvert
+from lalframe.utils import frtools
 
 from miyopy.utils.trillium import Trillium
 
-def read(fname):
-    
+def read(fname):    
     try:     
         data = FrequencySeries.read(fname,format='hdf5')
     except IOError as ioe:
@@ -26,97 +27,148 @@ def read(fname):
     
     return data
 
-'''
-chname3 list;
-'K1:PEM-IXV_GND_TR120Q_X_IN1_DQ',
-'K1:PEM-IXV_GND_TR120Q_Y_IN1_DQ',
-'K1:PEM-IXV_GND_TR120Q_Z_IN1_DQ',
-'K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ',
-'K1:PEM-IXV_GND_TR120QTEST_Y_IN1_DQ',
-'K1:PEM-IXV_GND_TR120QTEST_Z_IN1_DQ',
-'K1:PEM-EXV_GND_TR120Q_X_IN1_DQ',
-'K1:PEM-EXV_GND_TR120Q_Y_IN1_DQ',
-'K1:PEM-EXV_GND_TR120Q_Z_IN1_DQ',
-'K1:PEM-EYV_GND_TR120Q_X_IN1_DQ',
-'K1:PEM-EYV_GND_TR120Q_Y_IN1_DQ',
-'K1:PEM-EYV_GND_TR120Q_Z_IN1_DQ'
-'''
 
 import argparse 
 parser = argparse.ArgumentParser(description='description')
-parser.add_argument('dataname', help='help')
-parser.add_argument('--Plot')
+parser.add_argument('dataname',nargs='*', help='data name which you want to calculate')
+parser.add_argument('-specgram', action='store_true', help='If given, plot specgram')
 args = parser.parse_args()
-dataname = args.dataname
+datanames = args.dataname
+specgram = args.specgram
 
-hdf5_fmt = './{dataname}/Xaxis_{sensor}_50pct.hdf5'.replace("{dataname}",dataname)
-
-
-# Load FrequencySeries from saved hdf5 files.
-if True:
-    exv0 = read(hdf5_fmt.format(sensor='exv'))
-    ixv1 = read(hdf5_fmt.format(sensor='ixv1'))
-    ixv2 = read(hdf5_fmt.format(sensor='ixv2'))
-    d12 = read(hdf5_fmt.format(sensor='diff12'))
-    c12 = read(hdf5_fmt.format(sensor='comm12'))
-    x1500 = read(hdf5_fmt.format(sensor='x1500'))
-    strain = read(hdf5_fmt.format(sensor='strain'))
-    #strain = strain*550.0/strain.frequencies.value
-    # ??????????????????????????????????????????????????????????????
-    strain = strain#*3000 # ???????????????????????????
-    # ??????????????????????????????????????????????????????????????
+_t0 = {'chname3_1':'Dec 02 2018 11:00:00',
+      'chname3_2':'Jan 01 2019 04:00:00',
+      'chname3_3':'Jan 02 2019 00:00:00',
+      'chname4_1':'May 31 2019 00:00:00',
+      'chname4_2':'Jun 2 2019 00:00:00',
+      'chname4_3':'Jun 4 2019 00:00:00'}
     
 
+print(datanames)
+for dataname in datanames:
+    hdf5_fmt = './{dataname}/Xaxis_{sensor}_50pct.hdf5'.replace("{dataname}",dataname)
+    t0 = _t0[dataname]
 
-
-# Calcrate several noises
-if True:
-    ref = exv0
+    if specgram:
+        gwf_fmt = './{dataname}/X.gwf'.replace("{dataname}",dataname)
+        chname = frtools.get_channels(gwf_fmt)
         
-    _adcnoise = 2e-6*u.V*np.ones(len(ref)) # [V/rtHz]
-    _adcnoise2 = np.sqrt((10/2**15)**2/12/(214/2))*u.V*np.ones(len(ref)) # [V/rtHz]
-    _ampnoise = 6e-7*u.V*np.ones(len(ref)) # [V/rtHz]
-    _aanoise = 7e-8*u.V*np.ones(len(ref)) # [V/rtHz]
+        data = TimeSeriesDict.read('./{dataname}/X.gwf'.format(dataname=dataname),
+                                   chname,
+                                   format='gwf.lalframe',
+                                   pad=np.nan,
+                                   nproc=2,
+                                   verbose=True)
         
-    _adcnoise = FrequencySeries(_adcnoise, frequencies=ref.frequencies)
-    _adcnoise2 = FrequencySeries(_adcnoise2, frequencies=ref.frequencies)
-    _ampnoise = FrequencySeries(_ampnoise, frequencies=ref.frequencies)
-    _aanoise = FrequencySeries(_aanoise, frequencies=ref.frequencies)
-
-    amp = 10**(30/20.)
-    tr120q = Trillium('120QA')
-    tr240 = Trillium('240')
-    v2vel = tr120q.v2vel
-    v2vel = tr240.v2vel
+        # Override unit of data from count to voltage.
+        c2v = (10.0/2**15)*u.V/u.ct # [1]
+        print data   
+        exit()
     
-    adcnoise = v2vel(_adcnoise)/amp
-    ampnoise = v2vel(_ampnoise)/amp # 
-    aanoise = v2vel(_aanoise)/amp   
-    selfnoise_120q = tr120q.selfnoise()
-    selfnoise_240 = tr240.selfnoise()
+    # Load FrequencySeries from saved hdf5 files.
+    if True:
+        exv0 = read(hdf5_fmt.format(sensor='exv'))
+        ixv1 = read(hdf5_fmt.format(sensor='ixv1'))
+        ixv2 = read(hdf5_fmt.format(sensor='ixv2'))
+        d12 = read(hdf5_fmt.format(sensor='diff12'))*2
+        c12 = read(hdf5_fmt.format(sensor='comm12'))*2
+        d13 = read(hdf5_fmt.format(sensor='diff13'))*2   
+        c13 = read(hdf5_fmt.format(sensor='comm13'))*2
+        #x1500 = read(hdf5_fmt.format(sensor='x1500'))
+        #strain = read(hdf5_fmt.format(sensor='strain'))*3000
+        cdmr = c13/d13
+        #strain = strain*550.0/strain.frequencies.value
+        # ??????????????????????????????????????????????????????????????
+        #strain = strain#*3000 # ???????????????????????????
+        # ??????????????????????????????????????????????????????????????
+        ave = int(2**6*2)
+        bw = exv0.df       
 
+    # Calcrate several noises
+    if True:
+        ref = exv0        
+        _adcnoise = 2e-6*u.V*np.ones(len(ref)) # [V/rtHz]
+        _adcnoise2 = np.sqrt((10/2**15)**2/12/(214/2))*u.V*np.ones(len(ref)) # [V/rtHz]
+        _ampnoise = 6e-7*u.V*np.ones(len(ref)) # [V/rtHz]
+        _aanoise = 7e-8*u.V*np.ones(len(ref)) # [V/rtHz]       
+        _adcnoise = FrequencySeries(_adcnoise, frequencies=ref.frequencies)
+        _adcnoise2 = FrequencySeries(_adcnoise2, frequencies=ref.frequencies)
+        _ampnoise = FrequencySeries(_ampnoise, frequencies=ref.frequencies)
+        _aanoise = FrequencySeries(_aanoise, frequencies=ref.frequencies)        
+        amp = 10**(30/20.)
+        tr120q = Trillium('120QA')
+        tr240 = Trillium('240')
+        v2vel = tr120q.v2vel
+        v2vel = tr240.v2vel    
+        adcnoise = v2vel(_adcnoise)/amp
+        ampnoise = v2vel(_ampnoise)/amp # 
+        aanoise = v2vel(_aanoise)/amp   
+        selfnoise_120q = tr120q.selfnoise()
+        selfnoise_240 = tr240.selfnoise()
 
-# Plot
-if True:
-    from gwpy.plot import Plot
-    plot = Plot()
-    ax = plot.gca(xscale='log', xlim=(1e-3, 10), xlabel='Frequency [Hz]',
-                  yscale='log', ylim=(1e-12, 1e-4),
-                  ylabel=r'Velocity [m/sec/\rtHz]'
-                  )    
-    ax.loglog(x1500,label='x1500 (ref. GIF data)')    
-    ax.loglog(selfnoise_120q,'k--',label='Self Noise 120Q',linewidth=1,alpha=0.7)
-    ax.loglog(selfnoise_240,'m--',label='Self Noise 240',linewidth=1,alpha=0.7)
-    ax.loglog(adcnoise,'r--',label='ADC Noise',linewidth=1,alpha=0.7)
-    ax.loglog(ampnoise,'g--',label='Amp Noise',linewidth=1,alpha=0.7)
-    ax.loglog(aanoise,'b--',label='AA Noise',linewidth=1,alpha=0.7)        
-    ax.loglog(exv0,'k-',label='EXV')    
-    ax.loglog(ixv1,label='IXV1')
-    ax.loglog(ixv2,label='IXV2')
-    ax.loglog(d12,label='IXV1-IXV2')
-    ax.loglog(strain,label='StrainMeter')
-    #
-    ax.legend(fontsize=8,loc='lower left')    
-    ax.set_title('Seismometer, {dname}'.format(dname=dataname.replace('_','')),fontsize=16)
-    plot.savefig('./{dname}/ASD_{dname}_X.png'.format(dname=dataname))
-    print('save ./{dname}/ASD_{dname}_X.png'.format(dname=dataname))
+    # Plot
+    if True:
+        from gwpy.plot import Plot
+        plot = Plot()
+        ax = plot.gca(xscale='log', xlim=(1e-3, 10), xlabel='Frequency [Hz]',
+                    yscale='log', ylim=(1e-12, 1e-4),
+                    ylabel=r'Velocity [m/sec/\rtHz]'
+                    )    
+        #ax.loglog(x1500,label='x1500 (ref. GIF data)')    
+        ax.loglog(selfnoise_120q,'k--',label='Self Noise 120Q',linewidth=1,alpha=0.7)
+        ax.loglog(selfnoise_240,'m--',label='Self Noise 240',linewidth=1,alpha=0.7)
+        ax.loglog(adcnoise,'r--',label='ADC Noise',linewidth=1,alpha=0.7)
+        ax.loglog(ampnoise,'g--',label='Amp Noise',linewidth=1,alpha=0.7)
+        ax.loglog(aanoise,'b--',label='AA Noise',linewidth=1,alpha=0.7)        
+        ax.loglog(exv0,'k-',label='EXV')    
+        ax.loglog(ixv1,label='IXV1')
+        #ax.loglog(ixv2,label='IXV2')
+        #ax.loglog(d12,label='IXV1-IXV2')
+        #ax.loglog(strain,label='StrainMeter')
+        #ImportError: No module named gwpy.plot
+
+        ax.legend(fontsize=8,loc='lower left')    
+        ax.set_title('Seismometer, {dname}'.format(dname=dataname.replace('_','')),fontsize=16)
+        plot.savefig('./{dname}/ASD_{dname}_X.png'.format(dname=dataname))
+        plot.close()
+        print('save ./{dname}/ASD_{dname}_X.png'.format(dname=dataname))
+
+#
+    f = np.logspace(-3,2,1e4)
+    w = 2.0*np.pi*f
+    L = 3000.0 # m
+    c_p = 5500.0 # m/sec
+    cdmr_p = lambda w,c: np.sqrt((1.0+np.cos(L*w/c))/(1.0-np.cos(L*w/c)))
+    cdmr_ps = lambda w,c: np.sqrt((1.0+np.real(jv(0,L*w/c)))/(1.0-np.real(jv(0,L*w/c))))
+    
+    if True:
+        from gwpy.plot import Plot
+        import matplotlib.pyplot as plt
+        fig, (ax0,ax1) = plt.subplots(2,1,figsize=(8,6),sharex=True)
+        plt.subplots_adjust(hspace=0.1)
+        ax0.set_ylabel(r'Velocity [m/sec/\rtHz]',fontsize=15)
+        ax0.set_xlim(1e-3, 10)        
+        ax0.set_ylim(1e-10, 5e-6)
+        ax0.loglog(d13,'r',label='IXV1-EXV')
+        ax0.loglog(c13,'k',label='IXV1+IXV3')
+        ax0.set_yticks([1e-10,1e-9,1e-8,1e-7,1e-6])
+        ax0.tick_params(which='minor',color='black',axis='y')
+        #ax0.set_yticklabels([1e-10,1e-9,1e-8,1e-7,1e-6],style="sci")
+        ax0.legend(fontsize=10,loc='upper right')        
+        ax1.loglog(cdmr,'k',label='Measurement',zorder=1)
+        ax1.loglog(f,cdmr_ps(w,c_p),'r--',label='Body waves model (5500 m/sec)')
+        ax1.loglog(f,cdmr_p(w,c_p),'b--',label='Body wave model (5500 m/sec)')
+        ax1.loglog(f,np.ones(10000),'g--',label='No correlation model',zorder=2)
+        ax1.text(11, 0.1, 'START : {0}'.format(t0), rotation=90,ha='left',va='bottom')
+        ax1.text(13, 0.1, 'BW : {0:2.2e}, Window : hanning, AVE : {1}'.format(bw,ave),
+                 rotation=90,ha='left',va='bottom')        
+        ax1.legend(fontsize=10,loc='upper right')
+        ax1.set_ylim(1e-1, 1e3)
+        ax1.set_xlabel('Frequency [Hz]')        
+        ax1.set_ylabel(r'CDMR',fontsize=15)
+        ax1.set_xlim(1e-3, 10)        
+        ax0.set_title('Seismometers, {dname}'.format(dname=dataname.replace('_','')),
+                    fontsize=20)
+        plt.savefig('./{dname}/CDMR_{dname}_X.png'.format(dname=dataname))
+        print('save ./{dname}/CDMR_{dname}_X.png'.format(dname=dataname))
+        
--- cdmr/make_hdf5.py
+++ cdmr/make_hdf5.py
@@ -37,6 +37,14 @@ def calc_asd(ts, fftlength=2**10):
     median_0,low_0,high_0 = sg.percentile(50), sg.percentile(5), sg.percentile(95)
     return median_0,low_0,high_0
 
+chname3_x = ['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ',
+             'K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ',
+             'K1:PEM-EXV_GND_TR120Q_X_IN1_DQ',]
+chname4_x = [
+    'K1:PEM-SEIS_IXV_GND_EW_IN1_DQ',
+    'K1:PEM-SEIS_EXV_GND_EW_IN1_DQ',
+    'K1:PEM-SEIS_EYV_GND_EW_IN1_DQ',
+    ]
 
 
 
@@ -51,54 +59,80 @@ dataname = args.dataname
 if dataname == 'chname3_1':
     start = tconvert('Dec 02 2018 11:00:00')
     end = start + 2**16
+    chname = chname3_x
 elif dataname == 'chname3_2':
     start = tconvert('Jan 01 2019 04:00:00')
     end = start + 2**16
+    chname = chname3_x    
 elif dataname == 'chname3_3':
     start = tconvert('Jan 02 2019 00:00:00')
     end = start + 2**16
+    chname = chname3_x
+elif dataname == 'chname4_1':
+    start = tconvert('May 31 2019 00:00:00')    
+    end = start + 2**16
+    chname = chname4_x
+elif dataname == 'chname4_2':
+    start = tconvert('Jun 2 2019 00:00:00')    
+    end = start + 2**16
+    chname = chname4_x    
 else:
     raise ValueError('Invalid data name')
 
 
 
-chname = ['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ',
-          'K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ',
-          'K1:PEM-EXV_GND_TR120Q_X_IN1_DQ',]
     
 data = TimeSeriesDict.read('./{dataname}/X.gwf'.format(dataname=dataname),chname,
                            format='gwf.lalframe',
                            start=start,
                            end=end,
                            pad=np.nan,
-                           nproc=5,
+                           nproc=2,
                            verbose=True)
-
+print('Read Done')
 
 # Override unit of data from count to voltage.
 c2v = (10.0/2**15)*u.V/u.ct # [1]
 
 # [1] In the case of differential output.
-exv_x = data['K1:PEM-EXV_GND_TR120Q_X_IN1_DQ']#.override_unit('ct')#*c2v
-ixv1_x = data['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ']#.override_unit('ct')#*c2v
-ixv2_x = data['K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ']#.override_unit('ct')#*c2v
+if 'chname3' in dataname:
+    exv_x = data['K1:PEM-EXV_GND_TR120Q_X_IN1_DQ']
+    ixv1_x = data['K1:PEM-IXV_GND_TR120Q_X_IN1_DQ']
+    ixv2_x = data['K1:PEM-IXV_GND_TR120QTEST_X_IN1_DQ']
+elif 'chname4' in dataname:
+    exv_x = data['K1:PEM-SEIS_EXV_GND_EW_IN1_DQ']
+    ixv1_x = data['K1:PEM-SEIS_IXV_GND_EW_IN1_DQ']
+    ixv2_x = data['K1:PEM-SEIS_EYV_GND_EW_IN1_DQ']
+   
 exv_x.override_unit('ct')#*c2v
 ixv1_x.override_unit('ct')#*c2v
-ixv2_x.override_unit('ct')#*c2v
+ixv2_x.override_unit('ct')#*c2v    
 exv_x = exv_x*c2v
 ixv1_x = ixv1_x*c2v
 ixv2_x = ixv2_x*c2v
+    
 
 # Calc differential and common components.
 diff12 = ixv1_x - ixv2_x
 comm12 = ixv1_x + ixv2_x
+diff13 = ixv1_x - exv_x
+comm13 = ixv1_x + exv_x
+print('calc in timeseries')
 
 # Calc asd
-median_0,low_0,high_0 = calc_asd(exv_x)
-median_1,low_1,high_1 = calc_asd(ixv1_x)
-median_2,low_2,high_2 = calc_asd(ixv2_x)
-median_d12,low_d12,high_d12 = calc_asd(diff12)
-median_c12,low_c12,high_c12 = calc_asd(comm12)
+median_0,low_0,high_0 = calc_asd(exv_x,fftlength=2**10)
+print('calc asd done 1')
+median_1,low_1,high_1 = calc_asd(ixv1_x,fftlength=2**10)
+print('calc asd done 2')
+median_2,low_2,high_2 = calc_asd(ixv2_x,fftlength=2**10)
+print('calc asd done 3')
+median_d12,low_d12,high_d12 = calc_asd(diff12,fftlength=2**10)
+print('calc asd done 4')
+median_c12,low_c12,high_c12 = calc_asd(comm12,fftlength=2**10)
+print('calc asd done 5')
+median_d13,low_d13,high_d13 = calc_asd(diff13,fftlength=2**10)
+print('calc asd done 6')
+median_c13,low_c13,high_c13 = calc_asd(comm13,fftlength=2**10)
 print('calc asd done')
 
 # Calibrate to velocity.    
@@ -111,51 +145,71 @@ median_1,low_1,high_1 = v2vel(median_1)/amp, v2vel(low_1)/amp, v2vel(high_1)/amp
 median_2,low_2,high_2 = v2vel(median_2)/amp, v2vel(low_2)/amp, v2vel(high_2)/amp
 median_d12,low_d12,high_d12 = v2vel(median_d12)/amp, v2vel(low_d12)/amp, v2vel(high_d12)/amp
 median_c12,low_c12,high_c12 = v2vel(median_c12)/amp, v2vel(low_c12)/amp, v2vel(high_c12)/amp
+median_d13,low_d13,high_d13 = v2vel(median_d13)/amp, v2vel(low_d13)/amp, v2vel(high_d13)/amp
+median_c13,low_c13,high_c13 = v2vel(median_c13)/amp, v2vel(low_c13)/amp, v2vel(high_c13)/amp
+
 
 
 # Save data
-def save(data,fname='tmp.hdf5'):
-    data.write(fname,format='hdf5')
+def save(data,fname='tmp.hdf5',overwrite=True):
+    data.write(fname,format='hdf5',overwrite=True)
     
 if True:
+    print('save')
     hdf5_fmt = './{dataname}/Xaxis_{sensor}_{pct}.hdf5'.replace("{dataname}",dataname)
     
     exv_fmt = hdf5_fmt.replace('{sensor}','exv')
     median_0.name = 'exv_x_50pct'
     low_0.name = 'exv_x_5pct'
     high_0.name = 'exv_x_95pct'    
-    save(median_0, fname=exv_fmt.format(pct='50pct'))
-    save(low_0, fname=exv_fmt.format(pct='5pct'))
-    save(high_0, fname=exv_fmt.format(pct='95pct'))
+    save(median_0, fname=exv_fmt.format(pct='50pct'),overwrite=True)
+    save(low_0, fname=exv_fmt.format(pct='5pct'),overwrite=True)
+    save(high_0, fname=exv_fmt.format(pct='95pct'),overwrite=True)
     
     ixv1_fmt = hdf5_fmt.replace('{sensor}','ixv1')    
     median_1.name = 'ixv1_x_50pct'
     low_1.name = 'ixv1_x_5pct'
     high_1.name = 'ixv1_x_95pct'
-    save(median_1, fname=ixv1_fmt.format(pct='50pct'))
-    save(low_1, fname=ixv1_fmt.format(pct='5pct'))
-    save(high_1, fname=ixv1_fmt.format(pct='95pct'))
+    save(median_1, fname=ixv1_fmt.format(pct='50pct'),overwrite=True)
+    save(low_1, fname=ixv1_fmt.format(pct='5pct'),overwrite=True)
+    save(high_1, fname=ixv1_fmt.format(pct='95pct'),overwrite=True)
 
     ixv2_fmt = hdf5_fmt.replace('{sensor}','ixv2')    
     median_2.name = 'ixv2_x_50pct'
     low_2.name = 'ixv2_x_5pct'
     high_2.name = 'ixv2_x_95pct'
-    save(median_2, fname=ixv2_fmt.format(pct='50pct'))
-    save(low_2, fname=ixv2_fmt.format(pct='5pct'))
-    save(high_2, fname=ixv2_fmt.format(pct='95pct'))    
+    save(median_2, fname=ixv2_fmt.format(pct='50pct'),overwrite=True)
+    save(low_2, fname=ixv2_fmt.format(pct='5pct'),overwrite=True)
+    save(high_2, fname=ixv2_fmt.format(pct='95pct'),overwrite=True)    
     
     diff12_fmt = hdf5_fmt.replace('{sensor}','diff12')    
     median_d12.name = 'diff12_x_50pct'
     low_d12.name = 'diff12_x_5pct'
     high_d12.name = 'diff12_x_95pct'
-    save(median_d12, fname=diff12_fmt.format(pct='50pct'))
-    save(low_d12, fname=diff12_fmt.format(pct='5pct'))
-    save(high_d12, fname=diff12_fmt.format(pct='95pct'))    
+    save(median_d12, fname=diff12_fmt.format(pct='50pct'),overwrite=True)
+    save(low_d12, fname=diff12_fmt.format(pct='5pct'),overwrite=True)
+    save(high_d12, fname=diff12_fmt.format(pct='95pct'),overwrite=True)    
 
     comm12_fmt = hdf5_fmt.replace('{sensor}','comm12')    
     median_c12.name = 'comm12_x_50pct'
     low_c12.name = 'comm12_x_5pct'
     high_c12.name = 'comm12_x_95pct'
-    save(median_c12, fname=comm12_fmt.format(pct='50pct'))
-    save(low_c12, fname=comm12_fmt.format(pct='5pct'))
-    save(high_c12, fname=comm12_fmt.format(pct='95pct'))    
+    save(median_c12, fname=comm12_fmt.format(pct='50pct'),overwrite=True)
+    save(low_c12, fname=comm12_fmt.format(pct='5pct'),overwrite=True)
+    save(high_c12, fname=comm12_fmt.format(pct='95pct'),overwrite=True)    
+
+    diff13_fmt = hdf5_fmt.replace('{sensor}','diff13')    
+    median_d13.name = 'diff13_x_50pct'
+    low_d13.name = 'diff13_x_5pct'
+    high_d13.name = 'diff13_x_95pct'
+    save(median_d13, fname=diff13_fmt.format(pct='50pct'),overwrite=True)
+    save(low_d13, fname=diff13_fmt.format(pct='5pct'),overwrite=True)
+    save(high_d13, fname=diff13_fmt.format(pct='95pct'),overwrite=True)    
+
+    comm13_fmt = hdf5_fmt.replace('{sensor}','comm13')    
+    median_c13.name = 'comm13_x_50pct'
+    low_c13.name = 'comm13_x_5pct'
+    high_c13.name = 'comm13_x_95pct'
+    save(median_c13, fname=comm13_fmt.format(pct='50pct'),overwrite=True)
+    save(low_c13, fname=comm13_fmt.format(pct='5pct'),overwrite=True)
+    save(high_c13, fname=comm13_fmt.format(pct='95pct'),overwrite=True)
