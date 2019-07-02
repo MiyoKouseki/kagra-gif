1f6226814b909f0912a1e00ca7638e6c975feb60
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-07-02 15:38:53 +0900

    Made seismic noise spectra in 1 year

--- seismicnoise/plot_seismicnoise.py
+++ seismicnoise/plot_seismicnoise.py
@@ -7,51 +7,85 @@ from obspy.signal.spectral_estimation import get_nhnm, get_nlnm
 from gwpy.types.array2d import Array2D
 
 
-tr120 = Trillium('120QA')
-v2vel = tr120.v2vel    
+def peterson_noise_model(unit='um/sec'):
+    '''
+    Return 
     
-lt, ldb = get_nlnm()
-ht, hdb = get_nhnm()
-lfreq, lacc = 1./lt, 10**(ldb/20)*1e6
-hfreq, hacc = 1./ht, 10**(hdb/20)*1e6
-lfreq, lvel = lfreq, lacc/(2.0*np.pi*lfreq)
-hfreq, hvel = hfreq, hacc/(2.0*np.pi*hfreq)
-lfreq, ldisp = lfreq, lvel/(2.0*np.pi*lfreq)
-hfreq, hdisp = hfreq, hvel/(2.0*np.pi*hfreq)
+    '''
+    lt, ldb = get_nlnm()
+    ht, hdb = get_nhnm()
+    lfreq, lacc = 1./lt, 10**(ldb/20)*1e6
+    hfreq, hacc = 1./ht, 10**(hdb/20)*1e6
+        
+    if unit == 'um/sec':
+        lfreq, lvel = lfreq, lacc/(2.0*np.pi*lfreq)
+        hfreq, hvel = hfreq, hacc/(2.0*np.pi*hfreq)
+        return [[lfreq,lvel],[hfreq,hvel]]        
+    elif unit == 'um':
+        lfreq, ldisp = lfreq, lacc/(2.0*np.pi*lfreq)**2
+        hfreq, hdisp = hfreq, hacc/(2.0*np.pi*hfreq)**2     
+        return [[lfreq,ldisp],[hfreq,hdisp]]
+    else:
+        raise ValueError(unit)
 
-def percentile(sg_in1,pctl,unit='um'):    
+    
+def _percentile(axis,pctl=50,unit='um/sec',**kwargs):
+    _asd = FrequencySeries.read('./tmp/{0}_{1:02d}_LongTerm.hdf5'.format(axis,pctl))
     amp = 10**(30.0/20.0)
     c2v = 20.0/2**15    
-    _asd = v2vel(sg_in1.percentile(pctl))*c2v/amp*1e6
-    asd = _asd/(2.0*np.pi*_asd.frequencies.value)
+    _asd = v2vel(_asd)*c2v/amp*1e6
+    if unit=='um':
+        asd = _asd/(2.0*np.pi*_asd.frequencies.value)
+    elif unit=='um/sec':
+        asd = _asd
+    else:
+        raise ValueError('!!1')
     return asd
 
-if False:
-    print('Read spectrogram')
-    x = Spectrogram.read('./data2/SG_LongTerm_X.hdf5')
-    y = Spectrogram.read('./data2/SG_LongTerm_Y.hdf5')
-    z = Spectrogram.read('./data2/SG_LongTerm_Z.hdf5')
-    h = (x**2+y**2)**(0.5)
-else:
-    h = Spectrogram.read('./data2/SG_LongTerm_H.hdf5',format='hdf5')
-    #exit()
-    
-fig, ax = plt.subplots(1,1,figsize=(9,7))
-ax.plot_mmm(percentile(h,50),percentile(h,10),percentile(h,90))
-ax.loglog(lfreq,ldisp,'k--')
-ax.loglog(hfreq,hdisp,'k--',label='Peterson Low and High Noise Models')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('Frequency [Hz]')
-ax.set_ylabel('Displacement [um/rtHz]')
-ax.set_xlim(1e-2,8)
-ax.set_ylim(5e-6,20)
-ax.legend(fontsize=15,loc='lower left')
-import matplotlib.patches as patches
-r1 = patches.Rectangle(xy=(0.03, 0.01), width=0.07, height=10, ec='#000000', fill=False)
-r2 = patches.Rectangle(xy=(0.10, 0.01), width=0.2, height=10, ec='#000000', fill=False)
-ax.add_patch(r1)
-ax.add_patch(r2)
-plt.suptitle('Seismic noise of KAGRA',fontsize=40)
-plt.savefig('seismicnoise.png')
-plt.close()
+
+def percentile(axis,**kwargs):
+    if axis == 'H':
+        x = _percentile('X',**kwargs)
+        y = _percentile('Y',**kwargs)
+        return  (x**2 + y**2)**0.5        
+    elif axis in ['X','Y','Z']:
+        return _percentile(axis,**kwargs)
+    else:
+        raise ValueError('!!!')
+
+
+if __name__ == '__main__':
+    unit = 'um/sec'
+    #
+    tr120 = Trillium('120QA')
+    v2vel = tr120.v2vel    
+    #    
+    h50 = percentile('H',pctl=50,unit=unit)
+    h10 = percentile('H',pctl=10,unit=unit)
+    h90 = percentile('H',pctl=90,unit=unit)
+    z50 = percentile('Z',pctl=50,unit=unit)
+    z10 = percentile('Z',pctl=10,unit=unit)
+    z90 = percentile('Z',pctl=90,unit=unit)    
+    nlnm,nhnm = peterson_noise_model(unit=unit)
+    selfnoise = tr120.selfnoise(unit=unit.replace('um','m'))*1e6
+    #
+    fig, ax = plt.subplots(1,1,figsize=(9,7))
+    ax.plot_mmm(h50,h10,h90,label='Horizontal (10,50,90 percentile)',color='blue')
+    ax.plot_mmm(z50,z10,z90,label='Vertical (10,50,90 percentile)',color='red')
+    ax.loglog(selfnoise,'g--',label='Selfnoise of Seismometer',alpha=1.0)
+    ax.loglog(nlnm[0],nlnm[1],'k--')
+    ax.loglog(nhnm[0],nhnm[1],'k--',label='Peterson Low and High Noise Models')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_xlim(1e-2,8)
+    if unit=='um/sec':
+        ax.set_ylim(1e-4,20)
+        ax.set_ylabel('Velocity [um/sec/rtHz]')        
+    elif unit=='um':
+        ax.set_ylim(5e-6,20)
+        ax.set_ylabel('Displacement [um/rtHz]')
+    ax.legend(fontsize=15,loc='lower left')
+    plt.suptitle('Seismic Noise of KAGRA',fontsize=40)
+    plt.savefig('seismicnoise.png')
+    plt.close()
