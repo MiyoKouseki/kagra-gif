8e369029d969232ad1ae4cd86143402e7b9d1f24
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-07-19 14:09:58 +0900

    Update

--- underGroundMotion/phase_velocity/main.py
+++ underGroundMotion/phase_velocity/main.py
@@ -1,5 +1,8 @@
 import random
+import os
 import argparse
+import matplotlib
+matplotlib.use('agg')
 
 from gwpy.timeseries import TimeSeriesDict
 
@@ -19,13 +22,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     nproc = args.nproc
 
-
-    CHECK_BIT    = 0b1     # 1
-    LACK_OF_FILE = 0b10    # 2
-    LACK_OF_DATA = 0b100   # 4
-    GLITCH       = 0b1000  # 8
-    UNKNOWN      = 0b10000 # 16
-
     # Get segments    
     import numpy as np
     segments = np.loadtxt('hugo.txt',dtype='str')
@@ -36,47 +32,19 @@ if __name__ == "__main__":
         #              'and startgps>={0} and endgps<={1}'.format(args.start,args.end))
         use = db.ask('select startgps,endgps from EYV_SEIS WHERE flag=0 ' +
                      'and startgps>={0} and endgps<={1}'.format(args.start,args.end))
-        # for start,end in use:
-        #     log.debug(start)
-        #     db.update_flag('IXV_SEIS',start,end,CHECK_BIT,off=True)
-        # exit()
-        #
-        #
-        #db.add_table('EYV_SEIS')
-        #exit()
-        # for start,end,flag in segments:
-        #     start = int(start)
-        #     end = int(end)
-        #     #log.debug('{0} {1} {2}'.format(start,end,flag))
-        #     ans = db.get_flag('EYV_SEIS',start,end)
-        #     log.debug('##{0}'.format(ans))
-        #     if flag=='LACK_OF_FILE':
-        #         flag = LACK_OF_FILE
-        #     elif flag=='OK':
-        #         flag = CHECK_BIT
-        #     elif flag=='Unknown':
-        #         flag = UNKNOWN
-        #     else:
-        #         raise ValueError('!')
-        #     db.update_flag('EYV_SEIS',start,end,flag)
-        #     db.update_flag('EYV_SEIS',start,end,CHECK_BIT,off=True)
-        #     ans = db.get_flag('EYV_SEIS',start,end)
-        #     log.debug(ans)
-        #     #exit()
-        # exit()
 
     log.info('# ----------------------------------------')
     log.info('# Start SeismicNoise                      ')
     log.info('# ----------------------------------------')    
 
     #random.seed(3434)    
-    segments = use
-    #segments = random.sample(use,10)
+    #segments = use
+    segments = random.sample(use,3)
     n = len(segments)
     import traceback
     for i,(start,end) in enumerate(segments,1):
         sources = existedfilelist(start,end)
-        channels = get_seis_chname(start,end,place=['IXV'],axis=['X'])
+        channels = get_seis_chname(start,end,place=['IXV'],axis='all')
         try:
             data = TimeSeriesDict.read(sources,channels,nproc=nproc)
             data.crop(start,end)
@@ -90,14 +58,14 @@ if __name__ == "__main__":
             else:
                 log.debug(traceback.format_exc())
                 status = 'Unknown'
-                #exit()
+                exit()
         except TypeError as e:
             if 'NoneType' in e[0]:
                 status = 'LACK_OF_FILE'
             else:
                 log.debug(traceback.format_exc())
                 status = 'Unknown'
-                #exit()
+                exit()
         except IndexError as e:
             if 'cannot read TimeSeriesDict from empty source list' in e[0]:
                 status = 'LACK_OF_FILE'
@@ -108,11 +76,20 @@ if __name__ == "__main__":
             else:
                 log.debug(traceback.format_exc())
                 status = 'Unknown'
-                #exit()                
+                exit()                
         except Exception as e:
             log.debug(traceback.format_exc())
-            #exit()
+            exit()
             status = 'Unknown'
             
         log.debug('#7 {0:04d}/{4} {1} {2} {3}'.format(i,start,end,status,n))
-        # tail ./log/main.log | grep \#1 | awk '{print $3}' > hoge.txt
+        #log.debug(data)
+        plot = data.plot()
+
+        path = './data2/{0}'.format(str(start)[:5])
+        if not os.path.exists(path):
+            os.mkdir(path)
+        fname = path + '/{1}_{2}.png'.format(str(start)[:5],start,end)
+        plot.savefig(fname)
+        plot.close()
+        log.debug(fname)
