66aca563c56d8ed9916cb5db2290d0978936e53b
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-08-18 18:12:49 +0900

    Add coherence_check

--- armLengthCompensationSystem/coherence_check/_main.py
+++ armLengthCompensationSystem/coherence_check/_main.py
@@ -28,7 +28,6 @@ pdh   = data.values()[3]
 
 gif = gif.resample(512)
 pdh = pdh.resample(512)
-
 # Timeseries
 plot = pdh.plot(label=pdh.name.replace('_',' '),ylabel='Count?')
 ax = plot.gca()
@@ -42,52 +41,44 @@ diff = exv_x - ixv_x
 comm = exv_x + ixv_x
 
 # coherence
-csd_gif_diff = gif.csd(diff, fftlength=2**6, overlap=2**5)  # 2**7 = 128
-csd_pdh_gif  = pdh.csd(gif,  fftlength=2**6, overlap=2**5)  # 2**7 = 128
-csd_pdh_diff = pdh.csd(diff, fftlength=2**6, overlap=2**5)  # 2**7 = 128
-csd_pdh_comm = pdh.csd(comm, fftlength=2**6, overlap=2**5)  # 2**7 = 128
+coh_gif_diff = gif.coherence(diff, fftlength=2**6, overlap=2**5) # 2**7 = 128
+coh_gif_pdh = gif.coherence(pdh, fftlength=2**6, overlap=2**5)  # 2**7 = 128
+coh_pdh_diff = pdh.coherence(diff, fftlength=2**6, overlap=2**5)  # 2**7 = 128
+coh_pdh_comm = pdh.coherence(comm, fftlength=2**6, overlap=2**5)  # 2**7 = 128
 
 gif  =  gif.asd(fftlength=2**6, overlap=2**5)
 diff = diff.asd(fftlength=2**6, overlap=2**5)
 comm = comm.asd(fftlength=2**6, overlap=2**5)
 pdh  =  pdh.asd(fftlength=2**6, overlap=2**5)
-
-coh_gif_diff = csd_gif_diff/gif/(diff*1j)
-coh_pdh_gif  = csd_pdh_gif/pdh/gif
-coh_pdh_diff = csd_pdh_diff/pdh/(diff*1j)
-coh_pdh_comm = csd_pdh_comm/pdh/(comm*1j)
-
+gif = gif
 comm = comm/(2.0*np.pi*comm.frequencies.value)
 diff = diff/(2.0*np.pi*diff.frequencies.value)
 
+_csd = gif.csd(diff, fftlength=2**6, overlap=2**5) # 2**7 = 128
+_coh = _csd/gif/diff
+
 
 # plot Coherence
-fig, (ax0,ax1,ax2) = plt.subplots(3,1,figsize=(10,10))
+fig, (ax0,ax1) = plt.subplots(2,1,figsize=(10,7))
 ax0.loglog(gif,label='GIF',color='k')
 ax0.loglog(pdh,label='PDH (um?)',color='b',linestyle='-')
 ax0.loglog(diff,label='Seis Diff',color='r',linestyle='-')
 ax0.loglog(comm,label='Seis Comm',color='r',linestyle='--')
-ax0.legend(fontsize=15,loc='upper right')
+ax0.legend(fontsize=15,loc='upper tight')
 ax0.set_ylabel('Diplacement [um/rtHz]')
 ax0.set_ylim(1e-3,1e1)
 ax0.set_xlim(3e-2, 10)
-ax1.semilogx(coh_gif_diff.abs()**2,label='GIF vs Seis Diff',color='g',linestyle='-')
-ax1.semilogx(coh_pdh_gif.abs()**2,label='PDH vs GIF',color='k',linestyle='-')
-ax1.semilogx(coh_pdh_diff.abs()**2,label='PDH vs Diffs',color='b',linestyle='-')
-ax1.semilogx(coh_pdh_comm.abs()**2,label='PDH vs Comms',color='b',linestyle='--')
+ax1.semilogx(coh_gif_diff,label='GIF vs Seis Diff',color='g',linestyle='-')
+ax1.semilogx(_coh.abs(),label='GIF vs Seis Diff',color='g',linestyle='-')
+ax1.semilogx(coh_gif_pdh,label='PDH vs GIF',color='k',linestyle='-')
+ax1.semilogx(coh_pdh_diff,label='PDH vs Diffs',color='b',linestyle='-')
+ax1.semilogx(coh_pdh_comm,label='PDH vs Comms',color='b',linestyle='--')
 ax1.set_xlabel('Frequency [Hz]')
-ax1.set_ylabel('Magnitude-Squared \n Coherence')
+ax1.set_ylabel('Coherence')
 ax1.set_ylim(0, 1)
 ax1.set_xlim(3e-2, 10)
-ax1.legend(fontsize=15,loc='upper right')
-ax2.semilogx(coh_gif_diff.angle().rad2deg(),label='GIF vs Seis Diff',color='g',linestyle='-')
-ax2.semilogx(coh_pdh_gif.angle().rad2deg(),label='PDH vs GIF',color='k',linestyle='-')
-ax2.semilogx(coh_pdh_diff.angle().rad2deg(),label='PDH vs Diffs',color='b',linestyle='-')
-ax2.semilogx(coh_pdh_comm.angle().rad2deg(),label='PDH vs Comms',color='b',linestyle='--')
-ax2.set_ylim(-180, 180)
-ax2.set_yticks(range(-180, 181, 90))
-ax2.set_xlim(3e-2, 10)
-ax2.legend(fontsize=15,loc='upper right')
-ax2.set_ylabel('Phase [Deg.]')
+plt.legend(fontsize=15,loc='upper tight')
 plt.savefig('hoge.png')
 plt.close()
+
+
