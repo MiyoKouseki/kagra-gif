0409b4c79b6dca56bba2104aa924064223a80200
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-10-06 23:13:07 +0900

    Add run.sh to run with several arguments

--- ugm/seismic_response/main.py
+++ ugm/seismic_response/main.py
@@ -2,6 +2,12 @@
 #
 import numpy as np
 import matplotlib.pyplot as plt
+
+import argparse
+parser = argparse.ArgumentParser(description='This script is ...')
+parser.add_argument('tfs')
+args = parser.parse_args()
+
 start = 'Sep 06 00:30 JST'
 end = 'Sep 06 01:30 JST'
 
@@ -15,7 +21,7 @@ phi1500 = w*1500.0/(v)
 def tf_strain(tau,s):
     return (1-np.exp(-tau*s))*(v/s)
 
-def tf_disp(tau,s):
+def tf_disp(tau,s): #dL/u
     return (1-np.exp(-tau*s))
 
 
@@ -23,7 +29,9 @@ s = 1j*w
 
 
 tfs = ['gif2xarm','strain2gif','disp2gif']
+
 _tf = tfs[0]
+_tf = args.tfs
 
 fig,ax = plt.subplots(2,1,figsize=(6,5),sharex=True)
 
@@ -34,7 +42,7 @@ if _tf=='gif2xarm':
     ax[0].loglog(f,np.abs(H),color='k')
     ax[1].semilogx(f,np.rad2deg(np.angle(H)),color='k')    
     ax[0].set_ylim(1e-3,1e1)
-    fname = 'img_dLGIF2dLXARM.png'    
+    fname = 'img_dLGIF2dLXARM.png'
 elif _tf=='strain2gif':
     H1 = tf_strain(1500.0/v,s)
     ax[0].loglog(f,np.abs(H1),color='k',label='L=1500')
@@ -47,6 +55,7 @@ elif _tf=='strain2gif':
     ax[0].set_ylim(1e0,1e4)
     ax[0].legend()
     fname = 'img_strain2dL.png'
+    ax[0].set_ylabel('Magnitude [m]',fontsize=10)    
 elif _tf=='disp2gif':
     H1 = tf_disp(1500.0/v,s)
     ax[0].loglog(f,np.abs(H1),color='k',label='L=1500')
@@ -59,6 +68,7 @@ elif _tf=='disp2gif':
     label='G1500'
     ax[0].set_ylim(1e-3,1e1)
     fname = 'img_disp2dL.png'
+    ax[0].set_ylabel('Magnitude',fontsize=10)    
 else:
     raise ValueError('No Name')
 
@@ -74,8 +84,8 @@ ax[1].grid(linestyle='-', which='major', axis='both')
 ax[1].grid(linestyle='--', which='minor', axis='both')
 ax[0].yaxis.set_label_coords(-0.1,0.5)
 ax[1].yaxis.set_label_coords(-0.1,0.5)
-ax[0].set_ylabel('Magnitude',fontsize=10)
 ax[1].set_ylabel('Phase [Deg.]',fontsize=10)
 ax[1].set_xlabel('Frequency [Hz]',fontsize=10)
 plt.savefig(fname)
+print(fname)
 plt.close()
