019727a948fa88c5fc38dd7e7df53e3692e282c3
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-10-20 12:56:34 +0900

    Update main2

--- ugm/seismicnoise/main2_seismicnoise.py
+++ ugm/seismicnoise/main2_seismicnoise.py
@@ -17,7 +17,6 @@ def peterson_noise_model(unit='um/sec'):
     '''
     lt, ldb = get_nlnm()
     ht, hdb = get_nhnm()
-    print(lt)
     lfreq, lacc = 1./lt, 10**(ldb/20)*1e6
     hfreq, hacc = 1./ht, 10**(hdb/20)*1e6
         
@@ -33,8 +32,8 @@ def peterson_noise_model(unit='um/sec'):
         raise ValueError(unit)
 
     
-def _percentile(axis,pctl=50,unit='um/sec',suffix='',**kwargs):
-    _asd = FrequencySeries.read('./data2/LongTerm_gauss/LongTerm_{0}_{1}_{2}.hdf5'.format(axis,pctl,suffix))**0.5
+def _percentile(axis,pctl=50,unit='um',suffix='',_dir='LongTerm_gauss',**kwargs):
+    _asd = FrequencySeries.read('./data2/{3}/LongTerm_{0}_{1}_{2}.hdf5'.format(axis,pctl,suffix,_dir))**0.5
     amp = 10**(30.0/20.0)
     c2v = 20.0/2**15    
     _asd = v2vel(_asd)*c2v/amp*1e6
@@ -59,48 +58,73 @@ def percentile(axis,**kwargs):
     else:
         raise ValueError('!!!')
 
-
-def hoge(suffix='',plot_v=False,plot_99=True,unit='um',susresp=False,peterson=True,plot_selfnoise=True,plot_rms=False):
+def huge(axis,**kwargs):
+    h01 = percentile(axis,pctl=1 ,**kwargs)
+    h10 = percentile(axis,pctl=10,**kwargs)
+    h50 = percentile(axis,pctl=50,**kwargs)
+    h90 = percentile(axis,pctl=90,**kwargs)
+    h99 = percentile(axis,pctl=99,**kwargs)
+    return h01,h10,h50,h90,h99
+    
+def hoge(plot_v=False,plot_99=False,unit='um',peterson=True,plot_selfnoise=True,plot_rms=False,plot_h=False,fname_suffix='',day=False,night=False,**kwargs):
     #
-    #suffix = '1211817600_1245372032' # 1 year
-    #suffix = '1228594816_1232789120' # 12/11-
-    if susresp:
-        sus = FrequencySeries.read('./noctrl.hdf5').abs()
-    else:
-        sus = 1
-    h50 = percentile('H',pctl=50,unit=unit,suffix=suffix)
-    h50 = sus*h50
-    h50_rms = h50.rms()    
-    h01 = percentile('H',pctl=1,unit=unit,suffix=suffix)
-    h01 = sus*h01    
-    h99 = percentile('H',pctl=99,unit=unit,suffix=suffix)
-    h99 = sus*h99
-    h99_rms = h99.rms()
-    h10 = percentile('H',pctl=10,unit=unit,suffix=suffix)
-    h10 = sus*h10    
-    h90 = percentile('H',pctl=90,unit=unit,suffix=suffix)
-    h90 = sus*h90    
-    h90_rms = h90.rms()    
-    z50 = percentile('Z',pctl=50,unit=unit,suffix=suffix)
-    z01 = percentile('Z',pctl=1,unit=unit,suffix=suffix)
-    z10 = percentile('Z',pctl=10,unit=unit,suffix=suffix)
-    z99 = percentile('Z',pctl=99,unit=unit,suffix=suffix)
-    z90 = percentile('Z',pctl=90,unit=unit,suffix=suffix)
+    kwargs['unit'] = unit
+    kwargs['suffix'] = '1211817600_1245372032'
+    h01,h10,h50,h90,h99 = huge('H',_dir='LongTerm_gauss',**kwargs)
+    z01,z10,z50,z90,z99 = huge('Z',_dir='LongTerm_gauss',**kwargs)
+    h01_n,h10_n,h50_n,h90_n,h99_n = huge('H',_dir='LongTerm_gauss_night',**kwargs)
+    z01_n,z10_n,z50_n,z90_n,z99_n = huge('Z',_dir='LongTerm_gauss_night',**kwargs)
+    h01_d,h10_d,h50_d,h90_d,h99_d = huge('H',_dir='LongTerm_gauss_day',**kwargs)
+    z01_d,z10_d,z50_d,z90_d,z99_d = huge('Z',_dir='LongTerm_gauss_day',**kwargs)        
+    #
     nlnm,nhnm = peterson_noise_model(unit=unit)
     selfnoise = tr120.selfnoise(unit=unit.replace('um','m'))*1e6
     #
     fig, ax = plt.subplots(1,1,figsize=(10,8))
-    ax.plot_mmm(h50,h10,h90,label='Horizontal (10,50,90 percentile)',color='b')
-    if plot_99:
-        ax.plot_mmm(h50,h01,h99,label='Horizontal (1,50,99 percentile)',color='r',
-                    zorder=0)
-    if plot_rms:
-        ax.plot(h50_rms,'k--',zorder=3) 
-        ax.plot(h99_rms,'r--',zorder=3)
+
+    if plot_h:
+        if plot_v:
+            color = 'b'
+        else:
+            plt.suptitle('Horizontal Seismic Noise',fontsize=40)                      
+            color = 'k'
+        if (not night ) and (not day):
+            ax.plot_mmm(h50,h10,h90,label='Horizontal (10,50,90 percentile)',
+                        color=color,alpha=0.3)
+            if plot_99:
+                ax.plot_mmm(h50,h01,h99,label='Horizontal (1,50,99 percentile)',
+                            color=color,zorder=0,alpha=0.1)            
+        if night:
+            ax.plot_mmm(h50_n,h10_n,h90_n,label='Night (10,50,90 percentile)',
+                        color='b',alpha=0.3)
+        if day:
+            plt.suptitle('Night vs. Day (Horizontal)',fontsize=40)                
+            ax.plot_mmm(h50_d,h10_d,h90_d,label='Day (10,50,90 percentile)',
+                        color='r',alpha=0.3)            
+        if plot_rms:
+            ax.plot(h50_rms,'k--',zorder=3) 
+            ax.plot(h99_rms,'r--',zorder=3)        
     if plot_v:
-        ax.plot_mmm(z50,z10,z90,label='Vertical (10,50,90 percentile)',color='r')
-        if plot_99:                
-            ax.plot_mmm(z50,z01,z99,label='Vertical (1,50,99 percentile)',color='r')
+        if plot_h:
+            color = 'r'
+            plt.suptitle('Horizontal vs. Vertical',fontsize=40)            
+        else:
+            plt.suptitle('Vertical Seismic Noise',fontsize=40)            
+            color = 'k'
+        if (not night) and (not day):
+            ax.plot_mmm(z50,z10,z90,label='Vertical (10,50,90 percentile)',
+                        color=color,alpha=0.3)
+            if plot_99:                
+                ax.plot_mmm(z50,z01,z99,label='Vertical (1,50,99 percentile)',
+                            color=color,alpha=0.1)
+        if night:
+            ax.plot_mmm(z50_n,z10_n,z90_n,label='Night (10,50,90 percentile)',
+                        color='b',alpha=0.3)
+        if day:
+            plt.suptitle('Night vs. Day (Vertical)',fontsize=40)                        
+            ax.plot_mmm(z50_d,z10_d,z90_d,label='Day (10,50,90 percentile)',
+                        color='r',alpha=0.3)            
+            
     if plot_selfnoise:
         ax.loglog(selfnoise,'g--',label='Selfnoise of Seismometer',alpha=1.0)
     if peterson:
@@ -121,8 +145,7 @@ def hoge(suffix='',plot_v=False,plot_99=True,unit='um',susresp=False,peterson=Tr
     ax.legend(fontsize=18,loc='lower left')
     ax.grid(b=True,which='major', axis='both',linestyle='-')
     ax.grid(b=True,which='minor', axis='both',linestyle='--')    
-    plt.suptitle('Seismic Noise of KAGRA',fontsize=40)
-    fname = './results/seismicnoise_{0}.png'.format(suffix)
+    fname = './results/seismicnoise_{0}.png'.format(fname_suffix)
     print(fname)
     plt.savefig(fname)
     plt.close()
@@ -134,5 +157,8 @@ if __name__ == '__main__':
     suffix_list = map(lambda x: x.split('_99_')[1][:-5], files)
     suffix_list = filter(lambda x: '1211817600_1245372032' in x,suffix_list) # 1year
     for suffix in suffix_list:
-        #hoge(suffix=suffix,plot_v=False,plot_99=True,unit='um')
-        hoge(suffix=suffix,plot_v=True,plot_99=False,unit='um',plot_rms=False)
+        hoge(plot_v=True, plot_h=True, plot_99=False,fname_suffix='all_compare_v_vs_h')
+        hoge(plot_v=True, plot_h=False,plot_99=True, fname_suffix='all_v_99')
+        hoge(plot_v=False,plot_h=True, plot_99=True, fname_suffix='all_h_99')
+        hoge(plot_h=True, day=True,night=True,fname_suffix='h_compare_day_vs_night')
+        hoge(plot_v=True, day=True,night=True,fname_suffix='v_compare_day_vs_night')
