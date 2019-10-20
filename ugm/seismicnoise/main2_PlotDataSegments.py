5b464e55c638f28bfe3c2a4461c34b7a42a0876f
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-10-21 00:45:57 +0900

    Update

--- ugm/seismicnoise/main2_PlotDataSegments.py
+++ ugm/seismicnoise/main2_PlotDataSegments.py
@@ -21,19 +21,50 @@ if args.start!=1211817600 or args.end!=1245372032:
     
 
 from dataquality.dataquality import DataQuality
+fmt_total = 'select startgps,endgps from {2} WHERE ' +\
+  '(startgps>={0} and endgps<={1})'
+fmt_lack = 'select startgps,endgps from {2} WHERE (flag=2 or flag=4) and' +\
+  '(startgps>={0} and endgps<={1})'
+fmt_lack_day   = 'select startgps,endgps from {2} WHERE (flag=2 or flag=4) and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=0) and (((startgps-18)%86400)<43200)'
+fmt_lack_night = 'select startgps,endgps from {2} WHERE (flag=2 or flag=4) and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=43200) and (((startgps-18)%86400)<86400)'  
+fmt_gauss = 'select startgps,endgps from {2} WHERE flag=0 and ' +\
+  '(startgps>={0} and endgps<={1})'
+fmt_gauss_day   = 'select startgps,endgps from {2} WHERE flag=0 and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=0) and (((startgps-18)%86400)<43200)'
+fmt_gauss_night = 'select startgps,endgps from {2} WHERE flag=0 and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=43200) and (((startgps-18)%86400)<86400)'
+fmt_nonegauss = 'select startgps,endgps from {2} WHERE flag=8 and ' +\
+  '(startgps>={0} and endgps<={1})'
+fmt_nonegauss_day   = 'select startgps,endgps from {2} WHERE flag=8 and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=0) and (((startgps-18)%86400)<43200)'
+fmt_nonegauss_night = 'select startgps,endgps from {2} WHERE flag=8 and ' +\
+  '(startgps>={0} and endgps<={1}) and ' +\
+  '(((startgps-18)%86400)>=43200) and (((startgps-18)%86400)<86400)'
+  
 with DataQuality('./dataquality/dqflag.db') as db:
-    total      = db.ask('select startgps,endgps from EXV_SEIS')
-    normal     = db.ask('select startgps,endgps from EXV_SEIS WHERE flag=0')
-    lack       = db.ask('select startgps,endgps from EXV_SEIS WHERE flag=2 or flag=4')
-    non_normal = db.ask('select startgps,endgps from EXV_SEIS WHERE flag=8')
-    use        = db.ask('select startgps,endgps from EXV_SEIS WHERE flag=0 ' +
-                        'and startgps>={0} and endgps<={1}'.format(args.start,args.end))
-
+    total = db.ask(fmt_total.format(args.start,args.end,'EXV_SEIS'))
+    lack = db.ask(fmt_lack.format(args.start,args.end,'EXV_SEIS'))
+    lack_day = db.ask(fmt_lack_day.format(args.start,args.end,'EXV_SEIS'))
+    lack_night = db.ask(fmt_lack_night.format(args.start,args.end,'EXV_SEIS'))    
+    gauss = db.ask(fmt_gauss.format(args.start,args.end,'EXV_SEIS'))
+    gauss_day   = db.ask(fmt_gauss_day.format(args.start,args.end,'EXV_SEIS'))
+    gauss_night = db.ask(fmt_gauss_night.format(args.start,args.end,'EXV_SEIS'))
+    nonegauss = db.ask(fmt_nonegauss.format(args.start,args.end,'EXV_SEIS'))
+    nonegauss_day   = db.ask(fmt_nonegauss_day.format(args.start,args.end,'EXV_SEIS'))
+    nonegauss_night = db.ask(fmt_nonegauss_night.format(args.start,args.end,'EXV_SEIS'))            
 plot_pi = True
 if plot_pi:
-    data = [len(normal),len(non_normal),len(lack)]
+    data = [len(gauss),len(nonegauss),len(lack)]
+    #data = [len(gauss_day),len(nonegauss_day),len(lack_day)]
+    #data = [len(gauss_night),len(nonegauss_night),len(lack_night)]
     label = ['Gaussian','Non-Gaussian','Lack of Data']
-    #fig,ax=plt.subplots(1,1,figsize=(7,3),subplot_kw=dict(aspect="equal"))
     fig,ax=plt.subplots(1,1,figsize=(7,3))
     wedges, texts, autotexts = ax.pie(data,startangle=90,counterclock=False,
                                       wedgeprops={'linewidth':1, 'edgecolor':"black"},
@@ -48,19 +79,22 @@ if plot_pi:
 plot_segmentlist = True
 if plot_segmentlist:
     fname = './results/segmentlist.png'
+    gauss,lack,nonegauss = gauss,lack,nonegauss
+    #gauss,lack,nonegauss = gauss_day,lack_day,nonegauss_day    
+    #gauss,lack,nonegauss = gauss_night,lack_night,nonegauss_night
     start, end = total[0][0],total[-1][1]
     from gwpy.segments import DataQualityFlag
-    normal = DataQualityFlag(name='Gaussian ({0})'.format(len(normal)),
-                             active=normal,
+    gauss = DataQualityFlag(name='Gaussian ({0})'.format(len(gauss)),
+                             active=gauss,
                              known=[(start,end)])
     lack = DataQualityFlag(name='Lack of Data ({0})'.format(len(lack)),
                            active=lack,
                            known=[(start,end)])
-    non_normal = DataQualityFlag(name='Non-Gaussian ({0})'.format(len(non_normal)),
-                                 active=non_normal,known=[(start,end)])
+    nonegauss = DataQualityFlag(name='Non-Gaussian ({0})'.format(len(nonegauss)),
+                                 active=nonegauss,known=[(start,end)])
     total = DataQualityFlag(name='Total ({0})'.format(len(total)),
                             active=total,known=[(start,end)])
-    args = normal,non_normal,lack,total
+    args = gauss,nonegauss,lack,total
     start = args[0].known[0].start
     end = args[0].known[0].end
     plot = args[0].plot(figsize=(14,5),epoch=start,xlim=(start,end))
--- ugm/seismicnoise/main2_seismicnoise.py
+++ ugm/seismicnoise/main2_seismicnoise.py
@@ -3,18 +3,33 @@ import matplotlib.pyplot as plt
 from gwpy.frequencyseries import FrequencySeries
 from gwpy.spectrogram import Spectrogram
 from miyopy.utils.trillium import Trillium
-from obspy.signal.spectral_estimation import get_nhnm, get_nlnm
 from gwpy.types.array2d import Array2D
 
 tr120 = Trillium('120QA')
 v2vel = tr120.v2vel    
 
+#------------------------------------------------------------
+def peterson_noise_model(unit='um'):
+    ''' Return spectral density of the new seismic model of 
+    Peterson using Obspy package.
 
-def peterson_noise_model(unit='um/sec'):
-    '''
-    Return 
+    This function just converts spectral density as a 
+    function of frequencies from a function of period and 
+    also changes unit of the spectral acording to given 
+    option.
     
+    Parameters
+    ----------
+    unit : `str`, optional
+        default is um.
+
+    Returns
+    -------
+    val : list of `list`
+        return the low noise model and the high noise model.
+
     '''
+    from obspy.signal.spectral_estimation import get_nhnm, get_nlnm    
     lt, ldb = get_nlnm()
     ht, hdb = get_nhnm()
     lfreq, lacc = 1./lt, 10**(ldb/20)*1e6
@@ -47,7 +62,6 @@ def _percentile(axis,pctl=50,unit='um',suffix='',_dir='LongTerm_gauss',**kwargs)
         raise ValueError('!!1')
     return asd
 
-
 def percentile(axis,**kwargs):
     if axis == 'H':
         x = _percentile('X',**kwargs)
@@ -58,24 +72,44 @@ def percentile(axis,**kwargs):
     else:
         raise ValueError('!!!')
 
+def mean(axis,**kwargs):
+    if axis == 'H':
+        x = _mean('X',**kwargs)
+        y = _mean('Y',**kwargs)
+        return  (x**2 + y**2)**0.5        
+    elif axis in ['X','Y','Z']:
+        return _mean(axis,**kwargs)
+    else:
+        raise ValueError('!!!')
+
+    
 def huge(axis,**kwargs):
     h01 = percentile(axis,pctl=1 ,**kwargs)
     h10 = percentile(axis,pctl=10,**kwargs)
     h50 = percentile(axis,pctl=50,**kwargs)
     h90 = percentile(axis,pctl=90,**kwargs)
     h99 = percentile(axis,pctl=99,**kwargs)
-    return h01,h10,h50,h90,h99
+    h   = percentile(axis,pctl='mean',**kwargs)
+    return h01,h10,h50,h90,h99,h
     
-def hoge(plot_v=False,plot_99=False,unit='um',peterson=True,plot_selfnoise=True,plot_rms=False,plot_h=False,fname_suffix='',day=False,night=False,**kwargs):
+def hoge(plot_v=False,plot_99=False,unit='um',peterson=True,plot_selfnoise=True,plot_rms=False,plot_h=False,fname_suffix='',day=False,night=False,season=False,**kwargs):
     #
     kwargs['unit'] = unit
     kwargs['suffix'] = '1211817600_1245372032'
-    h01,h10,h50,h90,h99 = huge('H',_dir='LongTerm_gauss',**kwargs)
-    z01,z10,z50,z90,z99 = huge('Z',_dir='LongTerm_gauss',**kwargs)
-    h01_n,h10_n,h50_n,h90_n,h99_n = huge('H',_dir='LongTerm_gauss_night',**kwargs)
-    z01_n,z10_n,z50_n,z90_n,z99_n = huge('Z',_dir='LongTerm_gauss_night',**kwargs)
-    h01_d,h10_d,h50_d,h90_d,h99_d = huge('H',_dir='LongTerm_gauss_day',**kwargs)
-    z01_d,z10_d,z50_d,z90_d,z99_d = huge('Z',_dir='LongTerm_gauss_day',**kwargs)        
+    h01,h10,h50,h90,h99,h             = huge('H',_dir='LongTerm_gauss',**kwargs)
+    z01,z10,z50,z90,z99,z             = huge('Z',_dir='LongTerm_gauss',**kwargs)
+    h01_n,h10_n,h50_n,h90_n,h99_n,h_n = huge('H',_dir='LongTerm_gauss_night',**kwargs)
+    z01_n,z10_n,z50_n,z90_n,z99_n,z_n = huge('Z',_dir='LongTerm_gauss_night',**kwargs)
+    h01_d,h10_d,h50_d,h90_d,h99_d,h_d = huge('H',_dir='LongTerm_gauss_day',**kwargs)
+    z01_d,z10_d,z50_d,z90_d,z99_d,z_d = huge('Z',_dir='LongTerm_gauss_day',**kwargs)
+    h01_1,h10_1,h50_1,h90_1,h99_1,h_1 = huge('H',_dir='LongTerm_gauss_spring',**kwargs)
+    z01_1,z10_1,z50_1,z90_1,z99_1,z_1 = huge('Z',_dir='LongTerm_gauss_spring',**kwargs)
+    h01_2,h10_2,h50_2,h90_2,h99_2,h_2 = huge('H',_dir='LongTerm_gauss_summer',**kwargs)
+    z01_2,z10_2,z50_2,z90_2,z99_2,z_2 = huge('Z',_dir='LongTerm_gauss_summer',**kwargs)
+    h01_3,h10_3,h50_3,h90_3,h99_3,h_3 = huge('H',_dir='LongTerm_gauss_autumn',**kwargs)
+    z01_3,z10_3,z50_3,z90_3,z99_3,z_3 = huge('Z',_dir='LongTerm_gauss_autumn',**kwargs)
+    h01_4,h10_4,h50_4,h90_4,h99_4,h_4 = huge('H',_dir='LongTerm_gauss_winter',**kwargs)
+    z01_4,z10_4,z50_4,z90_4,z99_4,z_4 = huge('Z',_dir='LongTerm_gauss_winter',**kwargs)
     #
     nlnm,nhnm = peterson_noise_model(unit=unit)
     selfnoise = tr120.selfnoise(unit=unit.replace('um','m'))*1e6
@@ -87,13 +121,19 @@ def hoge(plot_v=False,plot_99=False,unit='um',peterson=True,plot_selfnoise=True,
             color = 'b'
         else:
             plt.suptitle('Horizontal Seismic Noise',fontsize=40)                      
-            color = 'k'
-        if (not night ) and (not day):
+            color = 'b'
+        if (not night ) and (not day) and (not season):
             ax.plot_mmm(h50,h10,h90,label='Horizontal (10,50,90 percentile)',
                         color=color,alpha=0.3)
             if plot_99:
                 ax.plot_mmm(h50,h01,h99,label='Horizontal (1,50,99 percentile)',
-                            color=color,zorder=0,alpha=0.1)            
+                            color=color,zorder=0,alpha=0.1)
+        if season:
+            ax.plot_mmm(h50_1,h10_1,h90_1,zorder=0,alpha=0.2,label='Spring',color='b')
+            #ax.plot_mmm(h50_2,h10_2,h90_2,zorder=0,alpha=0.2,label='Summer')
+            #ax.plot_mmm(h50_3,h10_3,h90_3,zorder=0,alpha=0.2,label='Autumn')
+            ax.plot_mmm(h50_4,h10_4,h90_4,zorder=0,alpha=0.2,label='Winter',color='r') 
+            plt.suptitle('Spring vs. Winter (Horizontal)',fontsize=40)      
         if night:
             ax.plot_mmm(h50_n,h10_n,h90_n,label='Night (10,50,90 percentile)',
                         color='b',alpha=0.3)
@@ -110,13 +150,19 @@ def hoge(plot_v=False,plot_99=False,unit='um',peterson=True,plot_selfnoise=True,
             plt.suptitle('Horizontal vs. Vertical',fontsize=40)            
         else:
             plt.suptitle('Vertical Seismic Noise',fontsize=40)            
-            color = 'k'
-        if (not night) and (not day):
+            color = 'r'
+        if (not night) and (not day) and (not season):
             ax.plot_mmm(z50,z10,z90,label='Vertical (10,50,90 percentile)',
                         color=color,alpha=0.3)
             if plot_99:                
                 ax.plot_mmm(z50,z01,z99,label='Vertical (1,50,99 percentile)',
                             color=color,alpha=0.1)
+        if season:
+            ax.plot_mmm(z50_1,z10_1,z90_1,zorder=0,alpha=0.2,label='Spring',color='b')
+            #ax.plot_mmm(z50_2,z10_2,z90_2,zorder=0,alpha=0.2,label='Summer')
+            #ax.plot_mmm(z50_3,z10_3,z90_3,zorder=0,alpha=0.2,label='Autumn')
+            ax.plot_mmm(z50_4,z10_4,z90_4,zorder=0,alpha=0.2,label='Winter',color='r') 
+            plt.suptitle('Spring vs. Winter (Vertical)',fontsize=40)            
         if night:
             ax.plot_mmm(z50_n,z10_n,z90_n,label='Night (10,50,90 percentile)',
                         color='b',alpha=0.3)
@@ -162,3 +208,5 @@ if __name__ == '__main__':
         hoge(plot_v=False,plot_h=True, plot_99=True, fname_suffix='all_h_99')
         hoge(plot_h=True, day=True,night=True,fname_suffix='h_compare_day_vs_night')
         hoge(plot_v=True, day=True,night=True,fname_suffix='v_compare_day_vs_night')
+        hoge(plot_h=True, season=True,fname_suffix='h_compare_season')
+        hoge(plot_v=True, season=True,fname_suffix='v_compare_season')
