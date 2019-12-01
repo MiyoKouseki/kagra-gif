525f6dc8e0acbfe2a7be2926ecd4ed497069dc56
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-12-02 03:34:46 +0900

    Add

--- ugm/strainmeter/main.py
+++ ugm/strainmeter/main.py
@@ -5,11 +5,13 @@ __author__ = 'Koseki Miyo'
 from gwpy.time import tconvert
 from gwpy.timeseries import TimeSeries,TimeSeriesDict
 from gwpy.spectrogram import Spectrogram
-
+import matplotlib.pyplot as plt
 import numpy as np
 import Kozapy.utils.filelist as existedfilelist
 
 
+def nan_helper(y):
+    return np.isnan(y), lambda z: z.nonzero()[0]
 
 start = tconvert('Nov 13 2019 00:00:00 JST')
 start = tconvert('Nov 16 2019 00:00:00 JST')
@@ -22,10 +24,17 @@ print(fnamelist)
 data = TimeSeriesDict.read(fnamelist,chname,nproc=8,pad=np.nan)
 #data = data.resample(32)
 #data = data.crop(start,end)
-plot = data.plot()
-plot.savefig('hoge.png')
-plot.close()
 gif = data['K1:GIF-X_STRAIN_OUT_DQ.mean']
+_x,y = gif.times,gif.value
+nans, x= nan_helper(y)
+y[nans] = np.interp(x(nans), x(~nans), y[~nans])
+plt.plot(_x,y)
+plt.savefig('hoge.png')
+plt.close()
+exit()
+# plot = gif.plot()
+# plot.savefig('hoge.png')
+# plot.close()
 #asd = gif.asd(fftlength=2**14,overlap=2**13)
 asd = gif.asd(fftlength=2**10,overlap=2**9)
 plot = asd.plot()
