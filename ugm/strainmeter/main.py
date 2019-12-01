65bbce681ca0a10d21dc2fbf4d5fdfb576d6780e
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-12-02 03:21:06 +0900

    Add

--- ugm/strainmeter/main.py
+++ ugm/strainmeter/main.py
@@ -9,6 +9,7 @@ from gwpy.spectrogram import Spectrogram
 import Kozapy.utils.filelist as existedfilelist
 
 start = tconvert('Nov 13 2019 00:00:00 JST')
+start = tconvert('Nov 16 2019 00:00:00 JST')
 end   = tconvert('Nov 20 2019 00:00:00 JST')
 #end   = tconvert('Nov 13 2019 01:00:00 JST')
 chname = ['K1:GIF-X_STRAIN_OUT_DQ.mean']
