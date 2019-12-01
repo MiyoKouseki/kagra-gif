0c3220c266b8a4382a9e83d965294fee58f07164
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-12-02 02:08:32 +0900

    add

---
+++ ugm/strainmeter/main.py
@@ -0,0 +1,21 @@
+#
+#! coding:utf-8
+__author__ = 'Koseki Miyo'
+
+from gwpy.time import tconvert
+from gwpy.timeseries import TimeSeries,TimeSeriesDict
+from gwpy.spectrogram import Spectrogram
+
+import Kozapy.utils.filelist as existedfilelist
+
+start = tconvert('Dec 01 2019 00:00:00 JST')
+end   = tconvert('Dec 01 2019 00:01:00 JST')
+chname = ['K1:GIF-X_STRAIN_OUT_DQ.mean']
+fnamelist = existedfilelist(start,end,trend='second')
+print(fnamelist)
+data = TimeSeriesDict.read(fnamelist,chname,nproc=2)
+#data = data.resample(32)
+#data = data.crop(start,end)
+plot = data.plot()
+plot.savefig('hoge.png')
+plot.close()
