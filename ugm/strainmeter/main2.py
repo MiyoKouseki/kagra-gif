d3e3372eeed6c0746ac90353fd03254d906b280d
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-12-02 16:28:56 +0900

    Update

--- ugm/strainmeter/main2.py
+++ ugm/strainmeter/main2.py
@@ -9,8 +9,8 @@ from miyopy.gif.datatype import GifData
 from gwpy.time import tconvert
 from gwpy.timeseries import TimeSeries
 
-start = tconvert('Sep 16 2019 20:16:00 UTC')
-end = tconvert('Sep 16 2019 21:10:00 UTC')
+start = tconvert('Sep 16 2019 20:26:00 UTC')
+end = tconvert('Sep 16 2019 21:00:00 UTC')
 if False:
     chname = 'CALC_STRAIN'
     gif = GifData('CALC_STRAIN')
@@ -23,20 +23,25 @@ if False:
     data.write('gif_Sep17.gwf')
 else:
     gif = TimeSeries.read('gif_Sep17.gwf','CALC_STRAIN')*3000*1e6
+    gif = gif - gif.crop(int(start),int(start)+1).mean().value
     fnamelist = existedfilelist(start,end,trend='full')
     c = 299792458 # m/sec
     lam = 1064e-9 # m
     xarm = TimeSeries.read(fnamelist,'K1:CAL-CS_PROC_XARM_FILT_AOM_OUT16')*3000.0/(c/lam)*1e6 # [um]
+    #print(xarm)
+    xarm = xarm - xarm.crop(int(start),int(start)+1).mean()
 
-fig,ax = plt.subplots(2,1,figsize=(10,10))
-ax[0].plot(gif,label='X-arm baseline length')
-ax[0].set_ylim(-10,10)
+    
+fig,ax = plt.subplots(2,1,figsize=(10,10),sharex=True)
+ax[0].plot(gif,label='X-arm baseline length',color='k')
+ax[0].set_ylim(-10,2)
 ax[0].set_xscale('auto-gps')
-ax[0].set_ylabel('Displacement [nm]')
-ax[1].plot(xarm,label='X-arm cavity length')
+ax[0].set_ylabel('Displacement [um]')
+ax[1].plot(xarm,label='X-arm cavity length',color='k')
 ax[1].set_xscale('auto-gps')
-ax[1].set_ylabel('Displacement [nm]')
-ax[1].set_ylim(-24,-14)
+ax[1].set_ylabel('Displacement [um]')
+ax[1].set_ylim(-10,2)
+ax[1].set_xlim(start,end)
 ax[0].legend()
 ax[1].legend()
 plt.savefig('gif.png')
