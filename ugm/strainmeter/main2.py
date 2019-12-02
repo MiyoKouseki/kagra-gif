73d3b30d0fdaf2841536600b33336ef275a270dc
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-12-02 15:29:21 +0900

    Update

--- ugm/strainmeter/main2.py
+++ ugm/strainmeter/main2.py
@@ -11,6 +11,7 @@ files = gif.findfiles(start,end,chname,prefix = '/Users/miyo/Dropbox/KagraData/g
 data = TimeSeries.read(files,name='CALC_STRAIN',format='gif')
 data = data.crop(start,end)
 data = data.resample(16)
+data.name = 'CALC_STRAIN'
 print(data)
 data.write('gif_Sep17.gwf')
 plot = data.plot()
